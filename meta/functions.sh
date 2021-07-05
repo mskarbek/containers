a4150266b8c6103bde8755b07c9dc162a8a8b709
Marcin Skarbek <git@skarbek.name>
2021-07-05 19:19:33 +0200

    feat: add common functions

---
+++ meta/functions.sh
@@ -0,0 +1,28 @@
+REGISTRY='10.88.0.252:8082'
+
+dnf_install () {
+    dnf -y \
+    --installroot=${CONTAINER_PATH} \
+    --releasever=8.4 \
+    --setopt=module_platform_id=platform:el8 \
+    --setopt=install_weak_deps=false \
+    --nodocs \
+    install ${1}
+}
+
+dnf_clean () {
+    dnf -y \
+    --installroot=${CONTAINER_PATH} \
+    --releasever=8.4 \
+    clean all
+}
+
+clean_files () {
+    rm -rvf \
+    ${CONTAINER_PATH}/var/cache/* \
+    ${CONTAINER_PATH}/var/log/dnf* \
+    ${CONTAINER_PATH}/var/log/yum* \
+    ${CONTAINER_PATH}/var/log/hawkey* \
+    ${CONTAINER_PATH}/var/log/rhsm \
+    ${CONTAINER_PATH}/etc/yum.repos.d/*
+}
