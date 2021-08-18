3b9383ad0d452ffeadd81797c723c6e95c6b2d8c
Marcin Skarbek <git@skarbek.name>
2021-08-18 15:12:03 +0200

    fix(base): add bootstrap case

--- base/build.sh
+++ base/build.sh
@@ -4,7 +4,12 @@ CONTAINER_UUID=$(cat /proc/sys/kernel/random/uuid)
 buildah from --name=${CONTAINER_UUID} ${REGISTRY}/micro:$(date +'%Y.%m.%d')-1
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
-dnf_install "systemd procps-ng dbus-broker"
+if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
+    cp -v /etc/yum.repos.d/redhat.repo ${CONTAINER_PATH}/etc/yum.repos.d/redhat.repo
+    dnf_install "systemd procps-ng"
+else
+    dnf_install "systemd procps-ng dbus-broker"
+fi
 
 dnf_clean
 
@@ -27,8 +32,12 @@ buildah run -t ${CONTAINER_UUID} systemctl mask\
  systemd-udev-trigger.service\
  systemd-udevd.service
 
-buildah run -t ${CONTAINER_UUID} systemctl enable\
- dbus-broker.service
+if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
+    rm -v ${CONTAINER_PATH}/etc/yum.repos.d/redhat.repo
+else
+    buildah run -t ${CONTAINER_UUID} systemctl enable\
+     dbus-broker.service
+fi
 
 clean_files
 
