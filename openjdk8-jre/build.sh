757f21debba894a90f3ab6e0236da68c95088c50
Marcin Skarbek <git@skarbek.name>
2021-08-18 15:47:50 +0200

    fix: add bootstrap image name

--- openjdk8-jre/build.sh
+++ openjdk8-jre/build.sh
@@ -32,7 +32,11 @@ buildah rm -a
 
 
 CONTAINER_UUID=$(cat /proc/sys/kernel/random/uuid)
-buildah from --name=${CONTAINER_UUID} ${REGISTRY}/base/openjdk8-jre:$(date +'%Y.%m.%d')-1
+if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
+    buildah from --name=${CONTAINER_UUID} ${REGISTRY}/bootstrap/base/openjdk8-jre:$(date +'%Y.%m.%d')-1
+else
+    buildah from --name=${CONTAINER_UUID} ${REGISTRY}/base/openjdk8-jre:$(date +'%Y.%m.%d')-1
+fi
 
 buildah config --cmd '[ "/usr/sbin/init" ]' ${CONTAINER_UUID}
 buildah config --stop-signal 'SIGRTMIN+3' ${CONTAINER_UUID}
