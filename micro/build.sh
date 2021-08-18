9cb5a7d67984919cd250fa5bd1bd14c00f133e2f
Marcin Skarbek <git@skarbek.name>
2021-08-18 14:23:00 +0200

    fix(micro): add ca-certs

--- micro/build.sh
+++ micro/build.sh
@@ -4,16 +4,13 @@ CONTAINER_UUID=$(cat /proc/sys/kernel/random/uuid)
 buildah from --name=${CONTAINER_UUID} scratch
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
-dnf_install "glibc-minimal-langpack coreutils-single"
+dnf_install "glibc-minimal-langpack coreutils-single ca-certificates"
 
 dnf_clean
 
 rsync_rootfs
 
-#chown -Rv root:root ${CONTAINER_PATH}/ect/pki/ca-cert/*
-#find ${CONTAINER_UUID}/ -type f -exec chmod -v 0755 {} \;
-
-#buildah run -t ${CONTAINER_UUID} update-ca-trust
+buildah run -t ${CONTAINER_UUID} update-ca-trust
 
 clean_files
 
