93d49bfcfd4f2b932ff888c228fc583e7b0416d1
Marcin Skarbek <git@skarbek.name>
2021-08-18 15:25:35 +0200

    fix(openjdk8-jre): add bootstrap case

--- openjdk8-jre/build.sh
+++ openjdk8-jre/build.sh
@@ -4,9 +4,18 @@ CONTAINER_UUID=$(cat /proc/sys/kernel/random/uuid)
 buildah from --name=${CONTAINER_UUID} ${REGISTRY}/base:$(date +'%Y.%m.%d')-1
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
-dnf_install "java-1.8.0-openjdk-headless tomcat-native apr"
+if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
+    cp -v /etc/yum.repos.d/redhat.repo ${CONTAINER_PATH}/etc/yum.repos.d/redhat.repo
+    dnf_install "java-1.8.0-openjdk-headless"
+else
+    dnf_install "java-1.8.0-openjdk-headless tomcat-native apr"
+fi
 
 dnf_clean
+
+if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
+    rm -v ${CONTAINER_PATH}/etc/yum.repos.d/redhat.repo
+fi
 
 clean_files
 
