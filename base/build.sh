5b00b6aa3709a3b145ff768b48b7f6657cd9ceab
Marcin Skarbek <git@skarbek.name>
2021-08-20 10:48:37 +0200

    refactor(base): move repo sync to separate, outside step

--- base/build.sh
+++ base/build.sh
@@ -9,31 +9,13 @@ fi
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
 if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
-    REPODIR="/var/tmp/$(cat /proc/sys/kernel/random/uuid)"
-    mkdir -vp ${REPODIR}
-
-    cat << EOF > /etc/yum.repos.d/hyperscale.repo
-[hyperscale]
-name=CentOS 8 Stream - Hyperscale
-baseurl=http://mirror.centos.org/centos/8-stream/hyperscale/x86_64/packages-main/
-enabled=1
-gpgcheck=0
-
-[hyperscale-facebook]
-name=CentOS 8 Stream - Hyperscale Facebook
-baseurl=http://mirror.centos.org/centos/8-stream/hyperscale/x86_64/packages-facebook/
-enabled=1
-gpgcheck=0
-EOF
-    
-    reposync -p ${REPODIR} --download-metadata --repo=hyperscale
-    reposync -p ${REPODIR} --download-metadata --repo=hyperscale-facebook
+    REPODIR="$(pwd)/files"
 
     cp -v /etc/yum.repos.d/redhat.repo ${CONTAINER_PATH}/etc/yum.repos.d/redhat.repo
         cat << EOF > ${CONTAINER_PATH}/etc/yum.repos.d/hyperscale.repo
 [hyperscale]
-name=CentOS 8 Stream - Hyperscale
-baseurl=file://${REPODIR}/hyperscale
+name=CentOS 8 Stream - Hyperscale Main
+baseurl=file://${REPODIR}/hyperscale-main
 enabled=1
 gpgcheck=0
 
@@ -47,7 +29,6 @@ EOF
     dnf_install "systemd procps-ng"
     
     rm -vf /etc/yum.repos.d/hyperscale.repo
-    rm -rf ${REPODIR}
 else
     dnf_install "systemd procps-ng dbus-broker"
 fi
