7fb8e9a7b45abf8d3792f19d515eb3d5da8fd349
Marcin Skarbek <git@skarbek.name>
2021-08-21 23:56:15 +0200

    feat(nginx): add simple nginx image

---
+++ nginx/build.sh
@@ -0,0 +1,33 @@
+. ../meta/functions.sh
+
+CONTAINER_UUID=$(cat /proc/sys/kernel/random/uuid)
+if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
+    buildah from --pull-never --name=${CONTAINER_UUID} ${REGISTRY}/bootstrap/systemd:$(date +'%Y.%m.%d')-1
+else
+    buildah from --pull-never --name=${CONTAINER_UUID} ${REGISTRY}/systemd:$(date +'%Y.%m.%d')-1
+fi
+CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
+
+if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
+    cp -v /etc/yum.repos.d/redhat.repo ${CONTAINER_PATH}/etc/yum.repos.d/redhat.repo
+fi
+
+dnf_module "disable php:7.2 nginx:1.14"
+dnf_module "enable nginx:1.18"
+dnf_install "nginx"
+
+dnf_clean
+
+if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
+    rm -v ${CONTAINER_PATH}/etc/yum.repos.d/redhat.repo
+fi
+
+buildah run -t ${CONTAINER_UUID} systemctl enable\
+ nginx.service
+
+if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
+    buildah commit ${CONTAINER_UUID} ${REGISTRY}/bootstrap/nginx:$(date +'%Y.%m.%d')-1
+else
+    buildah commit ${CONTAINER_UUID} ${REGISTRY}/nginx:$(date +'%Y.%m.%d')-1
+fi
+buildah rm -a
