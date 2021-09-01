326b9f93e437d5c546b25b0f8cd0f8ba515fb03c
Marcin Skarbek <git@skarbek.name>
2021-09-01 14:59:56 +0200

    fix: podman download script

--- meta/podman.sh
+++ meta/podman.sh
@@ -3,7 +3,7 @@
 VERSION="3.3.1"
 RELEASE="7.module_el8.5.0+914+45625a54"
 
-curl -L -O https://koji.mbox.centos.org/pkgs/packages/podman/${VERSION}/${RELEASE}/x86_64/podman-${VERSION}-${RELEASE}..x86_64.rpm
+curl -L -O https://koji.mbox.centos.org/pkgs/packages/podman/${VERSION}/${RELEASE}/x86_64/podman-${VERSION}-${RELEASE}.x86_64.rpm
 curl -L -O https://koji.mbox.centos.org/pkgs/packages/podman/${VERSION}/${RELEASE}/noarch/podman-docker-${VERSION}-${RELEASE}.noarch.rpm
 
 for PKG in {catatonit,gvproxy,plugins,remote,tests,catatonit-debuginfo,debuginfo,debugsource,gvproxy-debuginfo,plugins-debuginfo,remote-debuginfo}
