ab624882cdcd03a2424b2b33a791dfb574210937
Marcin Skarbek <git@skarbek.name>
2021-09-03 01:39:36 +0200

    feat(nodejs14): add image

---
+++ nodejs14/build.sh
@@ -0,0 +1,29 @@
+. ../meta/functions.sh
+
+CONTAINER_UUID=$(cat /proc/sys/kernel/random/uuid)
+buildah from --name=${CONTAINER_UUID} ${REGISTRY}/base:$(date +'%Y.%m.%d')-1
+CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
+
+dnf_cache
+
+dnf_module "enable nodejs:14"
+
+dnf_install "nodejs npm"
+
+dnf_clean
+dnf_clean_cache
+
+clean_files
+
+buildah commit ${CONTAINER_UUID} ${REGISTRY}/base/nodejs14:$(date +'%Y.%m.%d')-1
+buildah rm -a
+
+
+CONTAINER_UUID=$(cat /proc/sys/kernel/random/uuid)
+buildah from --name=${CONTAINER_UUID} ${REGISTRY}/base/nodejs14:$(date +'%Y.%m.%d')-1
+
+buildah config --cmd '[ "/usr/sbin/init" ]' ${CONTAINER_UUID}
+buildah config --stop-signal 'SIGRTMIN+3' ${CONTAINER_UUID}
+
+buildah commit ${CONTAINER_UUID} ${REGISTRY}/nodejs14:$(date +'%Y.%m.%d')-1
+buildah rm -a
