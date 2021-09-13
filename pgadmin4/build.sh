ae725a58a2848916d6b37f83b5c09844eccc560a
Marcin Skarbek <git@skarbek.name>
2021-09-13 12:42:15 +0200

    fix(pgadmin4): add missing dependencies

--- pgadmin4/build.sh
+++ pgadmin4/build.sh
@@ -7,7 +7,7 @@ CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 dnf_cache
 
 dnf_module "disable postgresql"
-dnf_install "--enablerepo=pgdg-common --enablerepo=pgdg13 --enablerepo=pgadmin4 pgadmin4-web"
+dnf_install "--enablerepo=pgdg-common --enablerepo=pgdg13 --enablerepo=pgadmin4 libstdc++ pgadmin4-web"
 
 dnf_clean
 dnf_clean_cache
