e187dcd2a6a281b317cba01a0434ae9d17c66bc0
Marcin Skarbek <git@skarbek.name>
2021-09-20 13:49:24 +0200

    feat(toolbox): add new images

---
+++ toolbox/build.sh
@@ -0,0 +1,27 @@
+. ../meta/functions.sh
+
+CONTAINER_UUID=$(cat /proc/sys/kernel/random/uuid)
+buildah from --name=${CONTAINER_UUID} ${REGISTRY}/base:$(date +'%Y.%m.%d')-1
+CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
+
+dnf_cache
+
+dnf_install "nano iputils iproute mtr htop tmux"
+
+dnf_clean
+dnf_clean_cache
+
+clean_files
+
+buildah commit ${CONTAINER_UUID} ${REGISTRY}/base/toolbox:$(date +'%Y.%m.%d')-1
+buildah rm -a
+
+
+CONTAINER_UUID=$(cat /proc/sys/kernel/random/uuid)
+buildah from --name=${CONTAINER_UUID} ${REGISTRY}/base/toolbox:$(date +'%Y.%m.%d')-1
+
+buildah config --cmd '[ "/usr/sbin/init" ]' ${CONTAINER_UUID}
+buildah config --stop-signal 'SIGRTMIN+3' ${CONTAINER_UUID}
+
+buildah commit ${CONTAINER_UUID} ${REGISTRY}/toolbox:$(date +'%Y.%m.%d')-1
+buildah rm -a
