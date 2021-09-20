3655a4101280f46d73d52b0b8bac40db54fa4247
Marcin Skarbek <git@skarbek.name>
2021-09-20 14:16:47 +0200

    chore: some stuff

--- meta/stuff.sh
+++ meta/stuff.sh
@@ -2,3 +2,4 @@
 
 sed -i 's/#mount_program = "\/usr\/bin\/fuse-overlayfs"/mount_program = "\/usr\/bin\/fuse-overlayfs"/' ${BUILD_CONTAINER_PATH}/rootfs/etc/containers/storage.conf
 
+rpm -r ${CONTAINER_PATH} -qa --qf '%{NAME}\t%{VERSION}\t%{RELEASE}\n'
--- meta/ubi_bootstrap.sh
+++ meta/ubi_bootstrap.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
-rm -rf /etc/rhsm-host
-rm -rf /etc/pki/entitlement-host
-subscription-manager register --auto-attach --release=8.4 --username=<user> --password=<password>
+. ./files/ENV
+
+VOL1_UUID=$(cat /proc/sys/kernel/random/uuid)
+VOL2_UUID=$(cat /proc/sys/kernel/random/uuid)
+zfs create -o mountpoint=legacy -o rootcontext="system_u:object_r:container_file_t:s0" ${ZFS_POOL}/datafs/var/lib/volumes/${VOL1_UUID}
+zfs create ${ZFS_POOL}/datafs/var/lib/volumes/${VOL2_UUID}
+ln -s /var/lib/volumes/${VOL2_UUID} /var/lib/volumes/builder-srv-data
+
+podman volume create --opt type=zfs --opt device=${ZFS_POOL}/datafs/var/lib/volumes/${VOL1_UUID} ${VOL1_UUID}
+
+podman pull registry.access.redhat.com/ubi8/ubi-init:8.4
+
+podman run -d --name=builder -v ${VOL1_UUID}:/var/lib/containers:z -v /var/lib/volumes/${VOL2_UUID}:/srv/data:z --privileged registry.access.redhat.com/ubi8/ubi-init:8.4
+
+podman exec builder rm -f /etc/rhsm-host /etc/pki/entitlement-host /etc/yum.repos.d/ubi.repo
+podman exec builder subscription-manager register --auto-attach --release=8.4 --username=${RHEL_USER} --password=${RHEL_PASS}
