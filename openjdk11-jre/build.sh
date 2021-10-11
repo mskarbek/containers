79538a505d91e4a8ebfec028e2096b9ffa47516d
Marcin Skarbek <git@skarbek.name>
2021-10-11 15:15:07 +0200

    feat(openjdk11-jre): add boostrap image

--- openjdk11-jre/build.sh
+++ openjdk11-jre/build.sh
@@ -1,27 +1,52 @@
 . ../meta/functions.sh
 
 CONTAINER_UUID=$(cat /proc/sys/kernel/random/uuid)
-buildah from --name=${CONTAINER_UUID} ${REGISTRY}/base:$(date +'%Y.%m.%d')-1
+if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
+    buildah from --pull-never --name=${CONTAINER_UUID} ${REGISTRY}/bootstrap/base:$(date +'%Y.%m.%d')-1
+else
+    buildah from --pull-never --name=${CONTAINER_UUID} ${REGISTRY}/base:$(date +'%Y.%m.%d')-1
+fi
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
 dnf_cache
 
-dnf_install "java-11-openjdk-headless tomcat-native apr"
+if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
+    cp -v /etc/yum.repos.d/redhat.repo ${CONTAINER_PATH}/etc/yum.repos.d/redhat.repo
+    dnf_install "java-11-openjdk-headless"
+else
+    dnf_install "java-11-openjdk-headless tomcat-native apr"
+fi
 
 dnf_clean
 dnf_clean_cache
 
+if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
+    rm -v ${CONTAINER_PATH}/etc/yum.repos.d/redhat.repo
+fi
+
 clean_files
 
-buildah commit ${CONTAINER_UUID} ${REGISTRY}/base/openjdk11-jre:$(date +'%Y.%m.%d')-1
+if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
+    buildah commit ${CONTAINER_UUID} ${REGISTRY}/bootstrap/base/openjdk11-jre:$(date +'%Y.%m.%d')-1
+else
+    buildah commit ${CONTAINER_UUID} ${REGISTRY}/base/openjdk11-jre:$(date +'%Y.%m.%d')-1
+fi
 buildah rm -a
 
 
 CONTAINER_UUID=$(cat /proc/sys/kernel/random/uuid)
-buildah from --name=${CONTAINER_UUID} ${REGISTRY}/base/openjdk11-jre:$(date +'%Y.%m.%d')-1
+if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
+    buildah from --pull-never --name=${CONTAINER_UUID} ${REGISTRY}/bootstrap/base/openjdk11-jre:$(date +'%Y.%m.%d')-1
+else
+    buildah from --pull-never --name=${CONTAINER_UUID} ${REGISTRY}/base/openjdk11-jre:$(date +'%Y.%m.%d')-1
+fi
 
 buildah config --cmd '[ "/usr/sbin/init" ]' ${CONTAINER_UUID}
 buildah config --stop-signal 'SIGRTMIN+3' ${CONTAINER_UUID}
 
-buildah commit ${CONTAINER_UUID} ${REGISTRY}/openjdk11-jre:$(date +'%Y.%m.%d')-1
+if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
+    buildah commit ${CONTAINER_UUID} ${REGISTRY}/bootstrap/openjdk11-jre:$(date +'%Y.%m.%d')-1
+else
+    buildah commit ${CONTAINER_UUID} ${REGISTRY}/openjdk11-jre:$(date +'%Y.%m.%d')-1
+fi
 buildah rm -a
