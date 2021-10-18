c3634fdc06d1547fc71143a5064f71ca3ee734f3
Marcin Skarbek <git@skarbek.name>
2021-10-18 03:29:21 +0200

    fix(knot): add temporary epel.repo

--- knot-dns/build.sh
+++ knot-dns/build.sh
@@ -1,17 +1,26 @@
 . ../meta/functions.sh
 
-CONTAINER_UUID=$(cat /proc/sys/kernel/random/uuid)
-buildah from --name=${CONTAINER_UUID} ${REGISTRY}/systemd:latest
+CONTAINER_UUID=$(create_container systemd:latest)
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
+
+if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
+    cp -v /etc/yum.repos.d/redhat.repo ${CONTAINER_PATH}/etc/yum.repos.d/redhat.repo
+fi
+
+cp -v ./files/epel.repo ${CONTAINER_PATH}/etc/yum.repos.d/epel.repo
 
 dnf_cache
 
-dnf_install "knot knot-module-dnstap"
+dnf_install "knot knot-module-dnstap knot-utils"
 
 dnf_clean
 dnf_clean_cache
 
 rsync_rootfs
+
+if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
+    rm -v ${CONTAINER_PATH}/etc/yum.repos.d/redhat.repo
+fi
 
 buildah run -t ${CONTAINER_UUID} systemctl enable\
  knot.service
@@ -20,5 +29,4 @@ clean_files
 
 buildah config --volume /var/lib/knot ${CONTAINER_UUID}
 
-buildah commit ${CONTAINER_UUID} ${REGISTRY}/knot-dns:latest
-buildah rm -a
+commit_container knot-dns:latest
--- knot-resolver/build.sh
+++ knot-resolver/build.sh
@@ -1,8 +1,13 @@
 . ../meta/functions.sh
 
-CONTAINER_UUID=$(cat /proc/sys/kernel/random/uuid)
-buildah from --name=${CONTAINER_UUID} ${REGISTRY}/systemd:latest
+CONTAINER_UUID=$(create_container systemd:latest)
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
+
+if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
+    cp -v /etc/yum.repos.d/redhat.repo ${CONTAINER_PATH}/etc/yum.repos.d/redhat.repo
+fi
+
+cp -v ./files/epel.repo ${CONTAINER_PATH}/etc/yum.repos.d/epel.repo
 
 dnf_cache
 
@@ -11,10 +16,13 @@ dnf_install "knot-resolver knot-resolver-module-dnstap knot-resolver-module-http
 dnf_clean
 dnf_clean_cache
 
+if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
+    rm -v ${CONTAINER_PATH}/etc/yum.repos.d/redhat.repo
+fi
+
 buildah run -t ${CONTAINER_UUID} systemctl enable\
  kresd@1.service
 
 clean_files
 
-buildah commit ${CONTAINER_UUID} ${REGISTRY}/knot-resolver:latest
-buildah rm -a
+commit_container knot-resolver:latest
