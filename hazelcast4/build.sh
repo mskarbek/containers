e1934005d5ceb40b1b9e8cc321e085d687249b9d
Marcin Skarbek <git@skarbek.name>
2021-10-27 11:41:17 +0200

    fix(hazelcast4): rename variable

--- hazelcast4/build.sh
+++ hazelcast4/build.sh
@@ -3,19 +3,19 @@
 CONTAINER_UUID=$(create_container openjdk11-jre:latest)
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
-MC_VERSION="4.2.2"
+HC_VERSION="4.2.2"
 
 TMP_DIR=$(mktemp -d)
 if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
-    tar xvf files/hazelcast-${MC_VERSION}.tar.gz -C ${TMP_DIR}
+    tar xvf files/hazelcast-${HC_VERSION}.tar.gz -C ${TMP_DIR}
 else
     pushd ${TMP_DIR}
-        curl -L https://download.hazelcast.com/download.jsp?version=hazelcast-${MC_VERSION}&type=tar&p=|tar xzv
+        curl -L https://download.hazelcast.com/download.jsp?version=hazelcast-${HC_VERSION}&type=tar&p=|tar xzv
     popd
 fi
 pushd ${CONTAINER_PATH}/usr/lib/
-    mv -v ${TMP_DIR}/hazelcast-${MC_VERSION} ./
-    ln -s hazelcast-${MC_VERSION} hazelcast
+    mv -v ${TMP_DIR}/hazelcast-${HC_VERSION} ./
+    ln -s hazelcast-${HC_VERSION} hazelcast
     rm -rf hazelcast/management-center
 popd
 rm -rf ${TMP_DIR}
