6e0d6cba9989efb9fddde35f3c8c2775c9380534
Marcin Skarbek <git@skarbek.name>
2021-11-02 11:14:03 +0100

    feat(openjdk17-jre): add new image

--- openjdk17-jre/build.sh
+++ openjdk17-jre/build.sh
@@ -1,27 +1,41 @@
 . ../meta/functions.sh
 
-CONTAINER_UUID=$(cat /proc/sys/kernel/random/uuid)
-buildah from --name=${CONTAINER_UUID} ${REGISTRY}/base:latest
+CONTAINER_UUID=$(create_container base:latest)
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
 dnf_cache
 
-dnf_install "java-latest-openjdk-headless tomcat-native apr"
+if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
+    cat << EOF > ${CONTAINER_PATH}/etc/yum.repos.d/beta.repo
+[rhel-8-beta-for-x86_64-baseos-rpms]
+name=Red Hat Enterprise Linux 8 Beta for x86_64 - BaseOS (RPMs)
+baseurl=file:///mnt/BaseOS/
+enabled=1
+gpgcheck=0
+
+[rhel-8-beta-for-x86_64-appstream-rpms]
+name=Red Hat Enterprise Linux 8 Beta for x86_64 - AppStream (RPMs)
+baseurl=file:///mnt/AppStream/
+enabled=1
+gpgcheck=0
+EOF
+    dnf_install "java-17-openjdk-headless"
+else
+    dnf_install "java-17-openjdk-headless tomcat-native apr"
+fi
 
 dnf_clean
 dnf_clean_cache
 
 clean_files
 
-buildah commit ${CONTAINER_UUID} ${REGISTRY}/base/openjdk16-jre:latest
-buildah rm -a
+commit_container base/openjdk17-jre:latest
 
 
-CONTAINER_UUID=$(cat /proc/sys/kernel/random/uuid)
-buildah from --name=${CONTAINER_UUID} ${REGISTRY}/base/openjdk16-jre:latest
+CONTAINER_UUID=$(create_container base/openjdk17-jre:latest)
+CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
 buildah config --cmd '[ "/usr/sbin/init" ]' ${CONTAINER_UUID}
 buildah config --stop-signal 'SIGRTMIN+3' ${CONTAINER_UUID}
 
-buildah commit ${CONTAINER_UUID} ${REGISTRY}/openjdk16-jre:latest
-buildah rm -a
+commit_container openjdk17-jre:latest
