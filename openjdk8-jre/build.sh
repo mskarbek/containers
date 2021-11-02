8a7274bb8c04d20dc0ca3f3b79a88c7b83a40dec
Marcin Skarbek <git@skarbek.name>
2021-11-02 12:11:29 +0100

    refactor(openjdk8-jre): update script using new common funcions

--- openjdk8-jre/build.sh
+++ openjdk8-jre/build.sh
@@ -1,11 +1,6 @@
 . ../meta/functions.sh
 
-CONTAINER_UUID=$(cat /proc/sys/kernel/random/uuid)
-if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
-    buildah from --pull-never --name=${CONTAINER_UUID} ${REGISTRY}/bootstrap/base:latest
-else
-    buildah from --pull-never --name=${CONTAINER_UUID} ${REGISTRY}/base:latest
-fi
+CONTAINER_UUID=$(create_container base:latest)
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
 dnf_cache
@@ -13,6 +8,7 @@ dnf_cache
 if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
     cp -v /etc/yum.repos.d/redhat.repo ${CONTAINER_PATH}/etc/yum.repos.d/redhat.repo
     dnf_install "java-1.8.0-openjdk-headless"
+    rm -v ${CONTAINER_PATH}/etc/yum.repos.d/redhat.repo
 else
     dnf_install "java-1.8.0-openjdk-headless tomcat-native apr"
 fi
@@ -20,33 +16,15 @@ fi
 dnf_clean
 dnf_clean_cache
 
-if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
-    rm -v ${CONTAINER_PATH}/etc/yum.repos.d/redhat.repo
-fi
-
 clean_files
 
-if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
-    buildah commit ${CONTAINER_UUID} ${REGISTRY}/bootstrap/base/openjdk8-jre:latest
-else
-    buildah commit ${CONTAINER_UUID} ${REGISTRY}/base/openjdk8-jre:latest
-fi
-buildah rm -a
+commit_container base/openjdk8-jre:latest
 
 
-CONTAINER_UUID=$(cat /proc/sys/kernel/random/uuid)
-if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
-    buildah from --pull-never --name=${CONTAINER_UUID} ${REGISTRY}/bootstrap/base/openjdk8-jre:latest
-else
-    buildah from --pull-never --name=${CONTAINER_UUID} ${REGISTRY}/base/openjdk8-jre:latest
-fi
+CONTAINER_UUID=$(create_container base/openjdk8-jre:latest)
+CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
 buildah config --cmd '[ "/usr/sbin/init" ]' ${CONTAINER_UUID}
 buildah config --stop-signal 'SIGRTMIN+3' ${CONTAINER_UUID}
 
-if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
-    buildah commit ${CONTAINER_UUID} ${REGISTRY}/bootstrap/openjdk8-jre:latest
-else
-    buildah commit ${CONTAINER_UUID} ${REGISTRY}/openjdk8-jre:latest
-fi
-buildah rm -a
+commit_container openjdk8-jre:latest
