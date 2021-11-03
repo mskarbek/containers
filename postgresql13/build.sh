8db7c8520fe256c5381fc5346329cc70aa50a05d
Marcin Skarbek <git@skarbek.name>
2021-11-03 13:56:57 +0100

    refactor(postgres13): update script with new common fnctions

--- postgresql13/build.sh
+++ postgresql13/build.sh
@@ -1,13 +1,12 @@
 . ../meta/functions.sh
 
-CONTAINER_UUID=$(cat /proc/sys/kernel/random/uuid)
-buildah from --pull-never --name=${CONTAINER_UUID} ${REGISTRY}/systemd:latest
+CONTAINER_UUID=$(create_container systemd:latest)
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
 dnf_cache
 
 dnf_module "disable postgresql"
-dnf_install "--enablerepo=pgdg-common --enablerepo=pgdg13 postgresql13-server postgresql13-contrib citus_13 pg_auto_failover_13"
+dnf_install "--enablerepo=pgdg-common --enablerepo=pgdg13 postgresql13-server postgresql13-contrib citus_13 pg_auto_failover_13 pg_qualstats_13 pg_stat_kcache_13 pg_wait_sampling_13 pg_track_settings13"
 
 dnf_clean
 dnf_clean_cache
@@ -17,5 +16,4 @@ buildah run -t ${CONTAINER_UUID} systemctl enable\
 
 clean_files
 
-buildah commit ${CONTAINER_UUID} ${REGISTRY}/postgres13:latest
-buildah rm -a
+commit_container postgres13:latest
