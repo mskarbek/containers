9c5cdc01f56621d7cf6bfa8fb1421e18e1ccd4d7
Marcin Skarbek <git@skarbek.name>
2021-11-12 18:03:03 +0100

    refactor(oppenssh): update script with new common functions

--- openssh/build.sh
+++ openssh/build.sh
@@ -1,38 +1,29 @@
 . ../meta/functions.sh
 
-CONTAINER_UUID=$(cat /proc/sys/kernel/random/uuid)
-if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
-    buildah from --pull-never --name=${CONTAINER_UUID} ${REGISTRY}/bootstrap/systemd:latest
-else
-    buildah from --pull-never --name=${CONTAINER_UUID} ${REGISTRY}/systemd:latest
-fi
+CONTAINER_UUID=$(create_container systemd:latest)
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
 dnf_cache
 
 if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
     cp -v /etc/yum.repos.d/redhat.repo ${CONTAINER_PATH}/etc/yum.repos.d/redhat.repo
+    dnf_install "openssh-server openssh-clients"
+    rm -v ${CONTAINER_PATH}/etc/yum.repos.d/redhat.repo
+else
+    dnf_install "openssh-server openssh-clients"
 fi
-
-dnf_install "openssh-server openssh-clients"
 
 dnf_clean
 dnf_clean_cache
 
-if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
-    rm -v ${CONTAINER_PATH}/etc/yum.repos.d/redhat.repo
-fi
+sed -i 's/session    required     pam_loginuid.so/#session    required     pam_loginuid.so/' ${CONTAINER_PATH}/etc/pam.d/sshd
 
 buildah run -t ${CONTAINER_UUID} systemctl unmask\
  systemd-logind.service
 
-sed -i 's/session    required     pam_loginuid.so/#session    required     pam_loginuid.so/' ${CONTAINER_PATH}/etc/pam.d/sshd
+buildah run -t ${CONTAINER_UUID} systemctl enable\
+ sshd.service
 
 clean_files
 
-if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
-    buildah commit ${CONTAINER_UUID} ${REGISTRY}/bootstrap/openssh:latest
-else
-    buildah commit ${CONTAINER_UUID} ${REGISTRY}/openssh:latest
-fi
-buildah rm -a
+commit_container openssh:latest
