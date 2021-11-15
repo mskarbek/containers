533bc62a69e7c5b2816bbad9d8639ab6e8b09680
Marcin Skarbek <git@skarbek.name>
2021-11-15 17:13:41 +0100

    chore(bootstrap): remove old scripts

--- meta/stuff.sh
+++
@@ -1,5 +0,0 @@
-#random shit
-
-sed -i 's/#mount_program = "\/usr\/bin\/fuse-overlayfs"/mount_program = "\/usr\/bin\/fuse-overlayfs"/' ${BUILD_CONTAINER_PATH}/rootfs/etc/containers/storage.conf
-
-rpm -r ${CONTAINER_PATH} -qa --qf '%{NAME}\t%{VERSION}\t%{RELEASE}\n'
