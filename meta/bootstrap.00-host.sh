ff0ec209e3a262f53e7af42294008d6c0e68d109
Marcin Skarbek <git@skarbek.name>
2021-11-15 18:27:28 +0100

    feat(bootstrap): add packages check

--- meta/bootstrap.00-host.sh
+++ meta/bootstrap.00-host.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 . ./files/ENV
 
+rpm -q zfs &> /dev/null && rpm -q skopeo &> /dev/null && rpm -q podman &> /dev/null || {
+    echo "Missing packages. Make sure that you have installed zfs, skopeo and podman."
+    exit 1
+}
+
 zfs list ${ZFS_POOL}/datafs/var/lib/containers &> /dev/null || {
     zfs list ${ZFS_POOL}/datafs/var/lib &> /dev/null || {
         zfs list ${ZFS_POOL}/datafs/var &> /dev/null || {
@@ -28,12 +33,12 @@ sed -i 's/driver = "overlay"/driver = "zfs"/' /etc/containers/storage.conf
 
 setsebool container_manage_cgroup on
 
-grep "systemd.unified_cgroup_hierarchy=1" /etc/sysconfig/grub || {
-    sed -i 's/\(GRUB_CMDLINE_LINUX="\)\(.*\)\("\)/\1\2systemd.unified_cgroup_hierarchy=1 \3/' /etc/sysconfig/grub
+grep "systemd.unified_cgroup_hierarchy=1" /etc/default/grub || {
+    sed -i 's/\(GRUB_CMDLINE_LINUX="\)\(.*\)\("\)/\1\2systemd.unified_cgroup_hierarchy=1 \3/' /etc/default/grub
     grub2-mkconfig -o /boot/efi/EFI/redhat/grub.cfg
 }
 
-grep "psi=1" /etc/sysconfig/grub || {
-    sed -i 's/\(GRUB_CMDLINE_LINUX="\)\(.*\)\("\)/\1\2 psi=1 \3/' /etc/sysconfig/grub
+grep "psi=1" /etc/default/grub || {
+    sed -i 's/\(GRUB_CMDLINE_LINUX="\)\(.*\)\("\)/\1\2 psi=1 \3/' /etc/default/grub
     grub2-mkconfig -o /boot/efi/EFI/redhat/grub.cfg
 }
