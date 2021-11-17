f03d9af74addfe2340f99218db8b7da41f0c25c7
Marcin Skarbek <git@skarbek.name>
2021-11-17 04:24:40 +0100

    feat(bootstrap): add minio as a storage to nexus

--- meta/nexus-bootstrap.sh
+++ meta/nexus-bootstrap.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-NEXUS_URL="http://repo.lab.skarbek.name"
+URL_MINIO="http://minio.lab.skarbek.name:9000"
+URL_NEXUS="http://repo.lab.skarbek.name:8081"
 
-while [ $(curl -s -o /dev/null -w '%{response_code}' -I "${NEXUS_URL}/service/rest/v1/status") != '200' ]; do
+MINIO_USER="Root-User"
+MINIO_PASS="Root-Pass"
+
+while [ $(curl -s -o /dev/null -w '%{response_code}' -I "${URL_NEXUS}/service/rest/v1/status") != '200' ]; do
     sleep 5
 done
 
@@ -11,14 +15,14 @@ NEW_PASS=$(cat /proc/sys/kernel/random/uuid)
 echo ${NEW_PASS} > files/admin.password.new
 
 curl -s -X 'PUT'\
- "${NEXUS_URL}/service/rest/v1/security/users/admin/change-password"\
+ "${URL_NEXUS}/service/rest/v1/security/users/admin/change-password"\
  -u admin:${OLD_PASS}\
  -H 'accept: application/json'\
  -H 'Content-Type: text/plain'\
  -d ${NEW_PASS}
 
 curl -s -X 'PUT' \
- "${NEXUS_URL}/service/rest/v1/security/anonymous"\
+ "${URL_NEXUS}/service/rest/v1/security/anonymous"\
  -u admin:${NEW_PASS}\
  -H 'accept: application/json'\
  -H 'Content-Type: application/json'\
@@ -29,7 +33,7 @@ curl -s -X 'PUT' \
 }'
 
 curl -X 'PUT'\
- "${NEXUS_URL}/service/rest/v1/security/realms/active"\
+ "${URL_NEXUS}/service/rest/v1/security/realms/active"\
  -u admin:${NEW_PASS}\
  -H 'accept: application/json'\
  -H 'Content-Type: application/json'\
@@ -40,62 +44,275 @@ curl -X 'PUT'\
 ]'
 
 curl -s -X 'DELETE'\
- "${NEXUS_URL}/service/rest/v1/repositories/maven-public"\
+ "${URL_NEXUS}/service/rest/v1/repositories/maven-public"\
  -u admin:${NEW_PASS}\
  -H 'accept: application/json'
 
 curl -s -X 'DELETE'\
- "${NEXUS_URL}/service/rest/v1/repositories/maven-central"\
+ "${URL_NEXUS}/service/rest/v1/repositories/maven-central"\
  -u admin:${NEW_PASS}\
  -H 'accept: application/json'
 
 curl -s -X 'DELETE'\
- "${NEXUS_URL}/service/rest/v1/repositories/maven-releases"\
+ "${URL_NEXUS}/service/rest/v1/repositories/maven-releases"\
  -u admin:${NEW_PASS}\
  -H 'accept: application/json'
 
 curl -s -X 'DELETE'\
- "${NEXUS_URL}/service/rest/v1/repositories/maven-snapshots"\
+ "${URL_NEXUS}/service/rest/v1/repositories/maven-snapshots"\
  -u admin:${NEW_PASS}\
  -H 'accept: application/json'
 
 curl -s -X 'DELETE'\
- "${NEXUS_URL}/service/rest/v1/repositories/nuget-group"\
+ "${URL_NEXUS}/service/rest/v1/repositories/nuget-group"\
  -u admin:${NEW_PASS}\
  -H 'accept: application/json'
 
 curl -s -X 'DELETE'\
- "${NEXUS_URL}/service/rest/v1/repositories/nuget.org-proxy"\
+ "${URL_NEXUS}/service/rest/v1/repositories/nuget.org-proxy"\
  -u admin:${NEW_PASS}\
  -H 'accept: application/json'
 
 curl -s -X 'DELETE'\
- "${NEXUS_URL}/service/rest/v1/repositories/nuget-hosted"\
+ "${URL_NEXUS}/service/rest/v1/repositories/nuget-hosted"\
+ -u admin:${NEW_PASS}\
+ -H 'accept: application/json'
+
+curl -s -X 'DELETE'\
+ "${URL_NEXUS}/service/rest/v1/blobstores/default"\
  -u admin:${NEW_PASS}\
  -H 'accept: application/json'
 
 REDHAT_PEM=$(curl -s -X 'GET'\
-  "${NEXUS_URL}/service/rest/v1/security/ssl?host=cdn.redhat.com&port=443"\
+  "${URL_NEXUS}/service/rest/v1/security/ssl?host=cdn.redhat.com&port=443"\
   -u admin:${NEW_PASS}\
   -H 'accept: application/json'|jq -r .pem)
 
 curl -vX 'POST' \
-  "${NEXUS_URL}/service/rest/v1/security/ssl/truststore"\
+  "${URL_NEXUS}/service/rest/v1/security/ssl/truststore"\
   -u admin:${NEW_PASS}\
   -H 'accept: application/json'\
   -H 'Content-Type: text/plain'\
   -d "${REDHAT_PEM}"
 
 curl -X 'POST'\
- "${NEXUS_URL}/service/rest/v1/repositories/docker/hosted"\
+ "${URL_NEXUS}/service/rest/v1/blobstores/s3"\
  -u admin:${NEW_PASS}\
  -H 'accept: application/json'\
  -H 'Content-Type: application/json'\
  -d '{
-  "name": "oci-hosted",
+  "name": "minio-oci-hosted-common",
+  "softQuota": null,
+  "bucketConfiguration": {
+    "bucket": {
+      "region": "eu-central-1",
+      "name": "nexus-oci-hosted-common",
+      "prefix": null,
+      "expiration": 3
+    },
+    "bucketSecurity": {
+      "accessKeyId": "'${MINIO_USER}'",
+      "secretAccessKey": "'${MINIO_PASS}'"
+    },
+    "advancedBucketConnection": {
+      "endpoint": "'${URL_MINIO}'",
+      "forcePathStyle": true
+    }
+  }
+}'
+
+curl -X 'POST'\
+ "${URL_NEXUS}/service/rest/v1/blobstores/s3"\
+ -u admin:${NEW_PASS}\
+ -H 'accept: application/json'\
+ -H 'Content-Type: application/json'\
+ -d '{
+  "name": "minio-yum-hosted-common",
+  "softQuota": null,
+  "bucketConfiguration": {
+    "bucket": {
+      "region": "eu-central-1",
+      "name": "nexus-yum-hosted-common",
+      "prefix": null,
+      "expiration": 3
+    },
+    "bucketSecurity": {
+      "accessKeyId": "'${MINIO_USER}'",
+      "secretAccessKey": "'${MINIO_PASS}'"
+    },
+    "advancedBucketConnection": {
+      "endpoint": "'${URL_MINIO}'",
+      "forcePathStyle": true
+    }
+  }
+}'
+
+curl -X 'POST'\
+ "${URL_NEXUS}/service/rest/v1/blobstores/s3"\
+ -u admin:${NEW_PASS}\
+ -H 'accept: application/json'\
+ -H 'Content-Type: application/json'\
+ -d '{
+  "name": "minio-yum-proxy-rhel8-8.5",
+  "softQuota": null,
+  "bucketConfiguration": {
+    "bucket": {
+      "region": "eu-central-1",
+      "name": "nexus-yum-proxy-rhel8-8.5",
+      "prefix": null,
+      "expiration": 3
+    },
+    "bucketSecurity": {
+      "accessKeyId": "'${MINIO_USER}'",
+      "secretAccessKey": "'${MINIO_PASS}'"
+    },
+    "advancedBucketConnection": {
+      "endpoint": "'${URL_MINIO}'",
+      "forcePathStyle": true
+    }
+  }
+}'
+
+curl -X 'POST'\
+ "${URL_NEXUS}/service/rest/v1/blobstores/s3"\
+ -u admin:${NEW_PASS}\
+ -H 'accept: application/json'\
+ -H 'Content-Type: application/json'\
+ -d '{
+  "name": "minio-yum-proxy-rhel8-layered",
+  "softQuota": null,
+  "bucketConfiguration": {
+    "bucket": {
+      "region": "eu-central-1",
+      "name": "nexus-yum-proxy-rhel8-layered",
+      "prefix": null,
+      "expiration": 3
+    },
+    "bucketSecurity": {
+      "accessKeyId": "'${MINIO_USER}'",
+      "secretAccessKey": "'${MINIO_PASS}'"
+    },
+    "advancedBucketConnection": {
+      "endpoint": "'${URL_MINIO}'",
+      "forcePathStyle": true
+    }
+  }
+}'
+
+curl -X 'POST'\
+ "${URL_NEXUS}/service/rest/v1/blobstores/s3"\
+ -u admin:${NEW_PASS}\
+ -H 'accept: application/json'\
+ -H 'Content-Type: application/json'\
+ -d '{
+  "name": "minio-yum-proxy-hyperscale",
+  "softQuota": null,
+  "bucketConfiguration": {
+    "bucket": {
+      "region": "eu-central-1",
+      "name": "nexus-yum-proxy-hyperscale",
+      "prefix": null,
+      "expiration": 3
+    },
+    "bucketSecurity": {
+      "accessKeyId": "'${MINIO_USER}'",
+      "secretAccessKey": "'${MINIO_PASS}'"
+    },
+    "advancedBucketConnection": {
+      "endpoint": "'${URL_MINIO}'",
+      "forcePathStyle": true
+    }
+  }
+}'
+
+curl -X 'POST'\
+ "${URL_NEXUS}/service/rest/v1/blobstores/s3"\
+ -u admin:${NEW_PASS}\
+ -H 'accept: application/json'\
+ -H 'Content-Type: application/json'\
+ -d '{
+  "name": "minio-yum-proxy-epel",
+  "softQuota": null,
+  "bucketConfiguration": {
+    "bucket": {
+      "region": "eu-central-1",
+      "name": "nexus-yum-proxy-epel",
+      "prefix": null,
+      "expiration": 3
+    },
+    "bucketSecurity": {
+      "accessKeyId": "'${MINIO_USER}'",
+      "secretAccessKey": "'${MINIO_PASS}'"
+    },
+    "advancedBucketConnection": {
+      "endpoint": "'${URL_MINIO}'",
+      "forcePathStyle": true
+    }
+  }
+}'
+
+curl -X 'POST'\
+ "${URL_NEXUS}/service/rest/v1/blobstores/s3"\
+ -u admin:${NEW_PASS}\
+ -H 'accept: application/json'\
+ -H 'Content-Type: application/json'\
+ -d '{
+  "name": "minio-yum-proxy-elrepo",
+  "softQuota": null,
+  "bucketConfiguration": {
+    "bucket": {
+      "region": "eu-central-1",
+      "name": "nexus-yum-proxy-elrepo",
+      "prefix": null,
+      "expiration": 3
+    },
+    "bucketSecurity": {
+      "accessKeyId": "'${MINIO_USER}'",
+      "secretAccessKey": "'${MINIO_PASS}'"
+    },
+    "advancedBucketConnection": {
+      "endpoint": "'${URL_MINIO}'",
+      "forcePathStyle": true
+    }
+  }
+}'
+
+curl -X 'POST'\
+ "${URL_NEXUS}/service/rest/v1/blobstores/s3"\
+ -u admin:${NEW_PASS}\
+ -H 'accept: application/json'\
+ -H 'Content-Type: application/json'\
+ -d '{
+  "name": "minio-yum-proxy-pgdg",
+  "softQuota": null,
+  "bucketConfiguration": {
+    "bucket": {
+      "region": "eu-central-1",
+      "name": "nexus-yum-proxy-pgdg",
+      "prefix": null,
+      "expiration": 3
+    },
+    "bucketSecurity": {
+      "accessKeyId": "'${MINIO_USER}'",
+      "secretAccessKey": "'${MINIO_PASS}'"
+    },
+    "advancedBucketConnection": {
+      "endpoint": "'${URL_MINIO}'",
+      "forcePathStyle": true
+    }
+  }
+}'
+
+curl -X 'POST'\
+ "${URL_NEXUS}/service/rest/v1/repositories/docker/hosted"\
+ -u admin:${NEW_PASS}\
+ -H 'accept: application/json'\
+ -H 'Content-Type: application/json'\
+ -d '{
+  "name": "oci-hosted-common",
   "online": true,
   "storage": {
-    "blobStoreName": "default",
+    "blobStoreName": "minio-oci-hosted-common",
     "strictContentTypeValidation": true,
     "writePolicy": "ALLOW"
   },
@@ -112,20 +329,43 @@ curl -X 'POST'\
 }'
 
 curl -X 'POST'\
- "${NEXUS_URL}/service/rest/v1/repositories/yum/proxy"\
+ "${URL_NEXUS}/service/rest/v1/repositories/yum/hosted"\
  -u admin:${NEW_PASS}\
  -H 'accept: application/json'\
  -H 'Content-Type: application/json'\
  -d '{
-  "name": "yum-proxy-rhel8-8.4",
+  "name": "yum-hosted-common",
   "online": true,
   "storage": {
-    "blobStoreName": "default",
+    "blobStoreName": "minio-yum-hosted-common",
+    "strictContentTypeValidation": true,
+    "writePolicy": "ALLOW_ONCE"
+  },
+  "cleanup": null,
+  "yum": {
+    "repodataDepth": 0,
+    "deployPolicy": "STRICT"
+  },
+  "component": {
+    "proprietaryComponents": false
+  }
+}'
+
+curl -X 'POST'\
+ "${URL_NEXUS}/service/rest/v1/repositories/yum/proxy"\
+ -u admin:${NEW_PASS}\
+ -H 'accept: application/json'\
+ -H 'Content-Type: application/json'\
+ -d '{
+  "name": "yum-proxy-rhel8-8.5",
+  "online": true,
+  "storage": {
+    "blobStoreName": "minio-yum-proxy-rhel8-8.5",
     "strictContentTypeValidation": true
   },
   "cleanup": null,
   "proxy": {
-    "remoteUrl": "https://cdn.redhat.com/content/dist/rhel8/8.4/x86_64/",
+    "remoteUrl": "https://cdn.redhat.com/content/dist/rhel8/8.5/x86_64/",
     "contentMaxAge": 1440,
     "metadataMaxAge": 180
   },
@@ -151,7 +391,7 @@ curl -X 'POST'\
 }'
 
 curl -X 'POST'\
- "${NEXUS_URL}/service/rest/v1/repositories/yum/proxy"\
+ "${URL_NEXUS}/service/rest/v1/repositories/yum/proxy"\
  -u admin:${NEW_PASS}\
  -H 'accept: application/json'\
  -H 'Content-Type: application/json'\
@@ -159,7 +399,7 @@ curl -X 'POST'\
   "name": "yum-proxy-rhel8-layered",
   "online": true,
   "storage": {
-    "blobStoreName": "default",
+    "blobStoreName": "minio-yum-proxy-rhel8-layered",
     "strictContentTypeValidation": true
   },
   "cleanup": null,
@@ -190,7 +430,7 @@ curl -X 'POST'\
 }'
 
 curl -X 'POST'\
- "${NEXUS_URL}/service/rest/v1/repositories/yum/proxy"\
+ "${URL_NEXUS}/service/rest/v1/repositories/yum/proxy"\
  -u admin:${NEW_PASS}\
  -H 'accept: application/json'\
  -H 'Content-Type: application/json'\
@@ -198,7 +438,7 @@ curl -X 'POST'\
   "name": "yum-proxy-hyperscale",
   "online": true,
   "storage": {
-    "blobStoreName": "default",
+    "blobStoreName": "minio-yum-proxy-hyperscale",
     "strictContentTypeValidation": true
   },
   "cleanup": null,
@@ -222,7 +462,7 @@ curl -X 'POST'\
 }'
 
 curl -X 'POST'\
- "${NEXUS_URL}/service/rest/v1/repositories/yum/proxy"\
+ "${URL_NEXUS}/service/rest/v1/repositories/yum/proxy"\
  -u admin:${NEW_PASS}\
  -H 'accept: application/json'\
  -H 'Content-Type: application/json'\
@@ -230,7 +470,7 @@ curl -X 'POST'\
   "name": "yum-proxy-epel",
   "online": true,
   "storage": {
-    "blobStoreName": "default",
+    "blobStoreName": "minio-yum-proxy-epel",
     "strictContentTypeValidation": true
   },
   "cleanup": null,
@@ -254,7 +494,7 @@ curl -X 'POST'\
 }'
 
 curl -X 'POST'\
- "${NEXUS_URL}/service/rest/v1/repositories/yum/proxy"\
+ "${URL_NEXUS}/service/rest/v1/repositories/yum/proxy"\
  -u admin:${NEW_PASS}\
  -H 'accept: application/json'\
  -H 'Content-Type: application/json'\
@@ -262,7 +502,7 @@ curl -X 'POST'\
   "name": "yum-proxy-elrepo",
   "online": true,
   "storage": {
-    "blobStoreName": "default",
+    "blobStoreName": "minio-yum-proxy-elrepo",
     "strictContentTypeValidation": true
   },
   "cleanup": null,
