104735912020216695c02bd9f1749b2aaadc7c11
Marcin Skarbek <git@skarbek.name>
2022-01-19 22:24:06 +0100

    refactor(openjdk8-jre): wip cleanup

--- openjdk8-jre/build.sh
+++ openjdk8-jre/build.sh
@@ -1,22 +1,16 @@
-. ../meta/functions.sh
+. ../meta/common.sh
 
 CONTAINER_UUID=$(create_container base:latest)
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
 dnf_cache
-
 if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
-    cp -v /etc/yum.repos.d/redhat.repo ${CONTAINER_PATH}/etc/yum.repos.d/redhat.repo
     dnf_install "java-1.8.0-openjdk-headless"
-    rm -v ${CONTAINER_PATH}/etc/yum.repos.d/redhat.repo
 else
     dnf_install "java-1.8.0-openjdk-headless tomcat-native apr"
 fi
-
-dnf_clean
 dnf_clean_cache
-
-clean_files
+dnf_clean
 
 commit_container base/openjdk8-jre:latest
 
