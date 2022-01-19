2e1965b0aea5355b0c26bc624ec3aeb8e560ff59
Marcin Skarbek <git@skarbek.name>
2022-01-19 22:51:25 +0100

    refactor(knot-resolver): wip cleanup

--- knot-resolver/build.sh
+++ knot-resolver/build.sh
@@ -1,28 +1,19 @@
-. ../meta/functions.sh
+. ../meta/common.sh
 
 CONTAINER_UUID=$(create_container systemd:latest)
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
-if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
-    cp -v /etc/yum.repos.d/redhat.repo ${CONTAINER_PATH}/etc/yum.repos.d/redhat.repo
+if [ ! -z ${IMAGE_BOOTSTRAP} ]; then
+    cp -v ./files/epel.repo ${CONTAINER_PATH}/etc/yum.repos.d/epel.repo
+    cp -v ./files/RPM-GPG-KEY-EPEL-8 /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-8
 fi
-
-cp -v ./files/epel.repo ${CONTAINER_PATH}/etc/yum.repos.d/epel.repo
 
 dnf_cache
-
 dnf_install "knot-resolver knot-resolver-module-dnstap knot-resolver-module-http"
-
-dnf_clean
 dnf_clean_cache
-
-if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
-    rm -v ${CONTAINER_PATH}/etc/yum.repos.d/redhat.repo
-fi
+dnf_clean
 
 buildah run -t ${CONTAINER_UUID} systemctl enable\
  kresd@1.service
-
-clean_files
 
 commit_container knot-resolver:latest
