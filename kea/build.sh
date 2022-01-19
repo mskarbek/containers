2e4769a35dab78764ab686f3c126899166442848
Marcin Skarbek <git@skarbek.name>
2022-01-19 23:54:14 +0100

    refactor(kea): wip cleanup

--- kea/build.sh
+++ kea/build.sh
@@ -1,30 +1,20 @@
-. ../meta/functions.sh
+. ../meta/common.sh
 
 CONTAINER_UUID=$(create_container systemd:latest)
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
+if [ ! -z ${IMAGE_BOOTSTRAP} ]; then
+    cp -v ./files/*.repo ${CONTAINER_PATH}/etc/yum.repos.d/
+    cp -v ./files/RPM-GPG-KEY-* /etc/pki/rpm-gpg/
+fi
+
 dnf_cache
-
-if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
-    cp -v /etc/yum.repos.d/redhat.repo ${CONTAINER_PATH}/etc/yum.repos.d/redhat.repo
-fi
-
-cp -v ./files/*.repo ${CONTAINER_PATH}/etc/yum.repos.d/
-cp -v ./files/RPM-GPG-KEY-* ${CONTAINER_PATH}/etc/pki/rpm-gpg/
-
 dnf_install "isc-kea isc-stork-agent"
-
-dnf_clean
 dnf_clean_cache
-
-if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
-    rm -v ${CONTAINER_PATH}/etc/yum.repos.d/redhat.repo
-fi
+dnf_clean
 
 buildah run -t ${CONTAINER_UUID} systemctl enable\
- isc-kea-dhcp4.service
+ kea-dhcp4.service
 # isc-stork-agent.service
-
-clean_files
 
 commit_container kea:latest
