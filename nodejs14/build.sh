0d5a08849f29088c61bc0d7633b148c355b96063
Marcin Skarbek <git@skarbek.name>
2022-01-20 00:41:08 +0100

    refactor(nodejs14): wip cleanup

--- nodejs14/build.sh
+++ nodejs14/build.sh
@@ -1,32 +1,12 @@
-. ../meta/functions.sh
+. ../meta/common.sh
 
 CONTAINER_UUID=$(create_container base:latest)
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
 dnf_cache
-
-if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
-    cp -v /etc/yum.repos.d/redhat.repo ${CONTAINER_PATH}/etc/yum.repos.d/redhat.repo
-    dnf_module "enable nodejs:14"
-    dnf_install "nodejs npm"
-    rm -v ${CONTAINER_PATH}/etc/yum.repos.d/redhat.repo
-else
-    dnf_module "enable nodejs:14"
-    dnf_install "nodejs npm"
-fi
-
-dnf_clean
+dnf_module "enable nodejs:14"
+dnf_install "nodejs npm"
 dnf_clean_cache
-
-clean_files
+dnf_clean
 
 commit_container base/nodejs14:latest
-
-
-CONTAINER_UUID=$(create_container base/nodejs14:latest)
-CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
-
-buildah config --cmd '[ "/usr/sbin/init" ]' ${CONTAINER_UUID}
-buildah config --stop-signal 'SIGRTMIN+3' ${CONTAINER_UUID}
-
-commit_container nodejs14:latest
