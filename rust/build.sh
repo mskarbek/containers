89d5b0581f79434e4a556cbfa22d628c8ce0220c
Marcin Skarbek <git@skarbek.name>
2022-01-20 00:46:40 +0100

    refactor(rust): wip cleanup

--- rust/build.sh
+++ rust/build.sh
@@ -1,17 +1,11 @@
-. ../meta/functions.sh
+. ../meta/common.sh
 
-CONTAINER_UUID=$(cat /proc/sys/kernel/random/uuid)
-buildah from --name=${CONTAINER_UUID} ${REGISTRY}/base:latest
+CONTAINER_UUID=$(create_container base:latest)
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
 dnf_cache
-
 dnf_install "rust cargo"
-
-dnf_clean
 dnf_clean_cache
+dnf_clean
 
-clean_files
-
-buildah commit ${CONTAINER_UUID} ${REGISTRY}/base/rust:latest
-buildah rm -a
+commit_container base/rust:latest
