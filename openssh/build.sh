d87e5a306579a4fccb2b7708d9e6654c65f3430d
Marcin Skarbek <git@skarbek.name>
2022-01-19 22:13:02 +0100

    refactor(openssh): wip cleanup

--- openssh/build.sh
+++ openssh/build.sh
@@ -1,29 +1,20 @@
-. ../meta/functions.sh
+. ../meta/common.sh
 
 CONTAINER_UUID=$(create_container systemd:latest)
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
-dnf_cache
-
-if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
-    cp -v /etc/yum.repos.d/redhat.repo ${CONTAINER_PATH}/etc/yum.repos.d/redhat.repo
-    dnf_install "openssh-server openssh-clients"
-    rm -v ${CONTAINER_PATH}/etc/yum.repos.d/redhat.repo
-else
-    dnf_install "openssh-server openssh-clients"
-fi
-
-dnf_clean
-dnf_clean_cache
-
-sed -i 's/session    required     pam_loginuid.so/#session    required     pam_loginuid.so/' ${CONTAINER_PATH}/etc/pam.d/sshd
-
 buildah run -t ${CONTAINER_UUID} systemctl unmask\
  systemd-logind.service
 
-buildah run -t ${CONTAINER_UUID} systemctl enable\
- sshd.service
+dnf_cache
+dnf_install "openssh-server openssh-clients"
+dnf_clean_cache
+dnf_clean
 
-clean_files
+for FILE in {sshd,remote,login,systemd-user}; do
+    sed -i 's/session[ \t]*required[ \t]*pam_loginuid.so/#session    required     pam_loginuid.so/' ${CONTAINER_PATH}/etc/pam.d/${FILE}
+done
+
+rsync_rootfs
 
 commit_container openssh:latest
