8588650690362aaacca79db9476ffaf1690e2ae0
Marcin Skarbek <git@skarbek.name>
2022-01-19 23:30:22 +0100

    refactor(fake-service): wip cleanup

--- fake-service/build.sh
+++ fake-service/build.sh
@@ -1,17 +1,11 @@
-. ../meta/functions.sh
+. ../meta/common.sh
+. ./files/VERSIONS
 
-CONTAINER_UUID=$(cat /proc/sys/kernel/random/uuid)
-if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
-    buildah from --pull-never --name=${CONTAINER_UUID} ${REGISTRY}/bootstrap/systemd:latest
-else
-    buildah from --pull-never --name=${CONTAINER_UUID} ${REGISTRY}/systemd:latest
-fi
+CONTAINER_UUID=$(create_container openjdk8-jre:latest)
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
-FAKE_SERVICE_VERSION=0.22.7
-
-if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
-    cp -v files/fake-service ${CONTAINER_PATH}/usr/local/bin/fake-service
+if [ -f ./files/fake-service ]; then
+    cp -v ./files/fake-service ${CONTAINER_PATH}/usr/local/bin/fake-service
 else
     TMP_DIR=$(mktemp -d)
     pushd ${TMP_DIR}
@@ -28,9 +22,6 @@ rsync_rootfs
 buildah run -t ${CONTAINER_UUID} systemctl enable\
  fake-service.service
 
-if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
-    buildah commit ${CONTAINER_UUID} ${REGISTRY}/bootstrap/fake-service:latest
-else
-    buildah commit ${CONTAINER_UUID} ${REGISTRY}/fake-service:latest
-fi
-buildah rm -a
+buildah config --volume /etc/fake-service ${CONTAINER_UUID}
+
+commit_container fake-service:latest
