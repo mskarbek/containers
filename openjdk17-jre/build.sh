446701519a07db777d95f189b8aed870da8fbad2
Marcin Skarbek <git@skarbek.name>
2022-01-19 22:24:22 +0100

    refactor(openjdk17-jre): wip cleanup

--- openjdk17-jre/build.sh
+++ openjdk17-jre/build.sh
@@ -1,34 +1,16 @@
-. ../meta/functions.sh
+. ../meta/common.sh
 
 CONTAINER_UUID=$(create_container base:latest)
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
 dnf_cache
-
 if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
-    cat << EOF > ${CONTAINER_PATH}/etc/yum.repos.d/beta.repo
-[rhel-8-beta-for-x86_64-baseos-rpms]
-name=Red Hat Enterprise Linux 8 Beta for x86_64 - BaseOS (RPMs)
-baseurl=file:///mnt/BaseOS/
-enabled=1
-gpgcheck=0
-
-[rhel-8-beta-for-x86_64-appstream-rpms]
-name=Red Hat Enterprise Linux 8 Beta for x86_64 - AppStream (RPMs)
-baseurl=file:///mnt/AppStream/
-enabled=1
-gpgcheck=0
-EOF
     dnf_install "java-17-openjdk-headless"
-    rm -f ${CONTAINER_PATH}/etc/yum.repos.d/beta.repo
 else
     dnf_install "java-17-openjdk-headless tomcat-native apr"
 fi
-
-dnf_clean
 dnf_clean_cache
-
-clean_files
+dnf_clean
 
 commit_container base/openjdk17-jre:latest
 
