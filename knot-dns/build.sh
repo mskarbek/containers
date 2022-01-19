118ce8ca5bfba71c0bce425d9392e260cec7efbe
Marcin Skarbek <git@skarbek.name>
2022-01-19 22:52:09 +0100

    refactor(knot-dns): wip cleanup

--- knot-dns/build.sh
+++ knot-dns/build.sh
@@ -1,32 +1,24 @@
-. ../meta/functions.sh
+. ../meta/common.sh
 
 CONTAINER_UUID=$(create_container systemd:latest)
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
-if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
-    cp -v /etc/yum.repos.d/redhat.repo ${CONTAINER_PATH}/etc/yum.repos.d/redhat.repo
+if [ ! -z ${IMAGE_BOOTSTRAP} ]; then
+    cp -v ./files/epel.repo ${CONTAINER_PATH}/etc/yum.repos.d/epel.repo
+    cp -v ./files/RPM-GPG-KEY-EPEL-8 /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-8
 fi
-
-cp -v ./files/epel.repo ${CONTAINER_PATH}/etc/yum.repos.d/epel.repo
 
 dnf_cache
-
 dnf_install "knot knot-module-dnstap knot-utils"
-
-dnf_clean
 dnf_clean_cache
+dnf_clean
 
 rsync_rootfs
-
-if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
-    rm -v ${CONTAINER_PATH}/etc/yum.repos.d/redhat.repo
-fi
 
 buildah run -t ${CONTAINER_UUID} systemctl enable\
  knot.service
 
-clean_files
-
+buildah config --volume /etc/knot ${CONTAINER_UUID}
 buildah config --volume /var/lib/knot ${CONTAINER_UUID}
 
 commit_container knot-dns:latest
