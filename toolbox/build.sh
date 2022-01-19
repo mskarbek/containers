150b160750b8025f3049d26f50ca4fc796b9268f
Marcin Skarbek <git@skarbek.name>
2022-01-19 22:14:36 +0100

    refactor(toolbox): wip cleanup

--- toolbox/build.sh
+++ toolbox/build.sh
@@ -1,51 +1,22 @@
-. ../meta/functions.sh
+. ../meta/common.sh
 
-CONTAINER_UUID=$(cat /proc/sys/kernel/random/uuid)
-if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
-    buildah from --pull-never --name=${CONTAINER_UUID} ${REGISTRY}/bootstrap/base:latest
-else
-    buildah from --pull-never --name=${CONTAINER_UUID} ${REGISTRY}/base:latest
-fi
+CONTAINER_UUID=$(create_container base:latest)
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
 dnf_cache
-
-if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
-    cp -v /etc/yum.repos.d/redhat.repo ${CONTAINER_PATH}/etc/yum.repos.d/redhat.repo
-fi
-
-dnf_install "nano iputils iproute mtr htop tmux lsof"
-
-dnf_clean
+dnf_install "curl vi nano telnet iputils iproute mtr tmux lsof"
 dnf_clean_cache
+dnf_clean
 
-clean_files
-
-if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
-    rm -v ${CONTAINER_PATH}/etc/yum.repos.d/redhat.repo
-fi
-
-if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
-    buildah commit ${CONTAINER_UUID} ${REGISTRY}/bootstrap/base/toolbox:latest
-else
-    buildah commit ${CONTAINER_UUID} ${REGISTRY}/base/toolbox:latest
-fi
-buildah rm -a
+commit_container base/toolbox:latest
 
 
-CONTAINER_UUID=$(cat /proc/sys/kernel/random/uuid)
-if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
-    buildah from --pull-never --name=${CONTAINER_UUID} ${REGISTRY}/bootstrap/base/toolbox:latest
-else
-    buildah from --pull-never --name=${CONTAINER_UUID} ${REGISTRY}/base/toolbox:latest
-fi
+CONTAINER_UUID=$(create_container openssh:latest)
+CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
-buildah config --cmd '[ "/usr/sbin/init" ]' ${CONTAINER_UUID}
-buildah config --stop-signal 'SIGRTMIN+3' ${CONTAINER_UUID}
+dnf_cache
+dnf_install "curl vi nano telnet iputils iproute mtr tmux lsof"
+dnf_clean_cache
+dnf_clean
 
-if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
-    buildah commit ${CONTAINER_UUID} ${REGISTRY}/bootstrap/toolbox:latest
-else
-    buildah commit ${CONTAINER_UUID} ${REGISTRY}/toolbox:latest
-fi
-buildah rm -a
+commit_container toolbox:latest
