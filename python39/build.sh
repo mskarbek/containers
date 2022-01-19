de06c7b8cdc247b21b10343216d9cd35ce0bfcf8
Marcin Skarbek <git@skarbek.name>
2022-01-20 00:52:16 +0100

    refactor(python39): wip cleanup

--- python39/build.sh
+++ python39/build.sh
@@ -1,27 +1,19 @@
-. ../meta/functions.sh
+. ../meta/common.sh
 
-CONTAINER_UUID=$(cat /proc/sys/kernel/random/uuid)
-buildah from --name=${CONTAINER_UUID} ${REGISTRY}/base:latest
+CONTAINER_UUID=$(create_container base:latest)
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
 dnf_cache
-
 dnf_install "python39 python39-pip python39-pip-wheel"
-
-dnf_clean
 dnf_clean_cache
+dnf_clean
 
-clean_files
-
-buildah commit ${CONTAINER_UUID} ${REGISTRY}/base/python39:latest
-buildah rm -a
+commit_container base/python39:latest
 
 
-CONTAINER_UUID=$(cat /proc/sys/kernel/random/uuid)
-buildah from --name=${CONTAINER_UUID} ${REGISTRY}/base/python39:latest
+CONTAINER_UUID=$(create_container base/python39:latest)
 
 buildah config --cmd '[ "/usr/sbin/init" ]' ${CONTAINER_UUID}
 buildah config --stop-signal 'SIGRTMIN+3' ${CONTAINER_UUID}
 
-buildah commit ${CONTAINER_UUID} ${REGISTRY}/python39:latest
-buildah rm -a
+commit_container python39:latest
