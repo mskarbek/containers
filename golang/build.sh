d04f84f1042b144f30a239f5711165e548f95194
Marcin Skarbek <git@skarbek.name>
2022-01-20 00:46:49 +0100

    refactor(golang): wip cleanup

--- golang/build.sh
+++ golang/build.sh
@@ -1,27 +1,11 @@
-. ../meta/functions.sh
+. ../meta/common.sh
 
-CONTAINER_UUID=$(cat /proc/sys/kernel/random/uuid)
-buildah from --name=${CONTAINER_UUID} ${REGISTRY}/base:latest
+CONTAINER_UUID=$(create_container base:latest)
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
 dnf_cache
-
 dnf_install "golang"
-
-dnf_clean
 dnf_clean_cache
+dnf_clean
 
-clean_files
-
-buildah commit ${CONTAINER_UUID} ${REGISTRY}/base/golang:latest
-buildah rm -a
-
-
-CONTAINER_UUID=$(cat /proc/sys/kernel/random/uuid)
-buildah from --name=${CONTAINER_UUID} ${REGISTRY}/base/golang:latest
-
-buildah config --cmd '[ "/usr/sbin/init" ]' ${CONTAINER_UUID}
-buildah config --stop-signal 'SIGRTMIN+3' ${CONTAINER_UUID}
-
-buildah commit ${CONTAINER_UUID} ${REGISTRY}/golang:latest
-buildah rm -a
+commit_container base/golang:latest
