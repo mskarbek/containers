f2deb69766a7daed354cb0d19270633c17fd5916
Marcin Skarbek <git@skarbek.name>
2022-01-20 00:52:10 +0100

    refactor(python36): wip cleanup

--- python36/build.sh
+++ python36/build.sh
@@ -1,27 +1,19 @@
-. ../meta/functions.sh
+. ../meta/common.sh
 
-CONTAINER_UUID=$(cat /proc/sys/kernel/random/uuid)
-buildah from --name=${CONTAINER_UUID} ${REGISTRY}/base:latest
+CONTAINER_UUID=$(create_container base:latest)
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
 dnf_cache
-
 dnf_install "python36 python3-pip python3-pip-wheel"
-
-dnf_clean
 dnf_clean_cache
+dnf_clean
 
-clean_files
-
-buildah commit ${CONTAINER_UUID} ${REGISTRY}/base/python36:latest
-buildah rm -a
+commit_container base/python36:latest
 
 
-CONTAINER_UUID=$(cat /proc/sys/kernel/random/uuid)
-buildah from --name=${CONTAINER_UUID} ${REGISTRY}/base/python36:latest
+CONTAINER_UUID=$(create_container base/python36:latest)
 
 buildah config --cmd '[ "/usr/sbin/init" ]' ${CONTAINER_UUID}
 buildah config --stop-signal 'SIGRTMIN+3' ${CONTAINER_UUID}
 
-buildah commit ${CONTAINER_UUID} ${REGISTRY}/python36:latest
-buildah rm -a
+commit_container python36:latest
