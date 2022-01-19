c6d2efbee11102483b9c5ed246bc927c332088b3
Marcin Skarbek <git@skarbek.name>
2022-01-19 23:37:49 +0100

    refactor(nexus): wip cleanup

--- nexus/build.sh
+++ nexus/build.sh
@@ -1,14 +1,16 @@
-. ../meta/functions.sh
+. ../meta/common.sh
+. ./files/VERSIONS
 
 CONTAINER_UUID=$(create_container openjdk8-jre:latest)
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
-if [[ -z ${NEXUS_VERSION} ]]; then
-    NEXUS_VERSION="3.36.0-01"
-fi
+dnf_cache
+dnf_install "curl"
+dnf_clean_cache
+dnf_clean
 
 TMP_DIR=$(mktemp -d)
-if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
+if [ -f "./files/nexus-${NEXUS_VERSION}-unix.tar.gz" ]; then
     tar xvf ./files/nexus-${NEXUS_VERSION}-unix.tar.gz -C ${TMP_DIR}
 else
     pushd ${TMP_DIR}
@@ -19,21 +21,22 @@ mkdir ${CONTAINER_PATH}/usr/lib/sonatype
 pushd ${CONTAINER_PATH}/usr/lib/sonatype
     mv -v ${TMP_DIR}/nexus-${NEXUS_VERSION} ./
     mv -v ${TMP_DIR}/sonatype-work ./
-    ln -s nexus-${NEXUS_VERSION} nexus
+    ln -s ./nexus-${NEXUS_VERSION} nexus
 popd
 rm -rf ${TMP_DIR}
 
 rsync_rootfs
 
-if [[ -f ./files/keystore.p12 && -f ./files/keystore.pass ]]; then
+sed -i 's/=\.\.\/sonatype-work/=\/var\/lib\/sonatype-work/' ${CONTAINER_PATH}/usr/lib/sonatype/nexus/bin/nexus.vmoptions
+if [ -f ./files/keystore.p12 ] && [ -f ./files/keystore.pass ]; then
     cp -v ./files/keystore.p12 ${CONTAINER_PATH}/usr/lib/sonatype/sonatype-work/nexus3/keystores/keystore.p12
-    sed -i 's/=\.\.\/sonatype-work/=\/var\/lib\/sonatype-work/' ${CONTAINER_PATH}/usr/lib/sonatype/nexus/bin/nexus.vmoptions
     echo "-Djavax.net.ssl.keyStore=/var/lib/sonatype-work/nexus3/keystores/keystore.p12" >> ${CONTAINER_PATH}/usr/lib/sonatype/nexus/bin/nexus.vmoptions
     echo "-Djavax.net.ssl.keyStorePassword=$(cat ./files/keystore.pass)" >> ${CONTAINER_PATH}/usr/lib/sonatype/nexus/bin/nexus.vmoptions
 fi
 
 buildah run -t ${CONTAINER_UUID} systemctl enable\
- nexus.service
+ nexus.service\
+ nexus-password.service
 
 buildah config --volume /var/lib/sonatype-work ${CONTAINER_UUID}
 
