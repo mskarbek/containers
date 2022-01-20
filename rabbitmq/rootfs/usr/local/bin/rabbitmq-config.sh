5cf4255e631e6893a973a2180e03b14423723cc5
Marcin Skarbek <git@skarbek.name>
2022-01-20 13:18:44 +0100

    fix(rabbitmq): fix password/config service

--- rabbitmq/rootfs/usr/local/bin/rabbitmq-config.sh
+++ rabbitmq/rootfs/usr/local/bin/rabbitmq-config.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
 
-for e in $(tr "\000" "\n" < /proc/1/environ); do
-    eval "export ${e}"
-done
-
 rabbitmq-plugins enable rabbitmq_managment
+rabbitmq-plugins enable rabbitmq_auth_backend_ldap
 rabbitmqctl add_user ${RABBITMQ_USER} ${RABBITMQ_PASS}
 rabbitmqctl set_permissions -p "/" ${RABBITMQ_USER} ".*" ".*" ".*"
 rabbitmqctl set_user_tags ${RABBITMQ_USER} administrator
 rabbitmqctl delete_user guest
-
-touch /var/lib/rabbitmq/.lock
