a1daa48aa88b47915c3975af80ef90f917276c47
Marcin Skarbek <git@skarbek.name>
2022-01-20 04:36:23 +0100

    refactor(mysql8): wip cleanup

--- mysql8/build.sh
+++ mysql8/build.sh
@@ -1,18 +1,14 @@
-. ../meta/functions.sh
+. ../meta/common.sh
 
-CONTAINER_ID=$(buildah from ${REGISTRY}/systemd:latest)
-CONTAINER_PATH=$(buildah mount ${CONTAINER_ID})
+CONTAINER_UUID=$(create_container systemd:latest)
+CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
-ln -s /etc/yum.repos.d/redhat.repo ${CONTAINER_PATH}/etc/yum.repos.d/host.repo
-
-dnf_install "mysql-server hostname"
-
+dnf_cache
+dnf_install "policycoreutils rsync tar gettext hostname bind-utils groff-base mysql-server"
+dnf_clean_cache
 dnf_clean
 
-buildah run -t ${CONTAINER_ID} systemctl enable\
+buildah run -t ${CONTAINER_UUID} systemctl enable\
  mysqld.service
 
-clean_files
-
-buildah commit ${CONTAINER_ID} ${REGISTRY}/mysql:latest
-buildah rm -a
+commit_container mysql8:latest
