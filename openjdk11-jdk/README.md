86f80052a7a0b7a488a4d419cfe3c85eeb3b1d62
Marcin Skarbek <git@skarbek.name>
2022-01-20 02:22:44 +0100

    refactor(openjdk11-jdk): wip cleanup

---
+++ openjdk11-jdk/README.md
@@ -0,0 +1,1 @@
+# openjdk11-jdk
--- openjdk11-jdk/build.sh
+++ openjdk11-jdk/build.sh
@@ -1,32 +1,12 @@
-. ../meta/functions.sh
+. ../meta/common.sh
 
 CONTAINER_UUID=$(create_container base/openjdk11-jre:latest)
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
 dnf_cache
-
-if [[ ! -z ${IMAGE_BOOTSTRAP} ]]; then
-    cp -v /etc/yum.repos.d/redhat.repo ${CONTAINER_PATH}/etc/yum.repos.d/redhat.repo
-    dnf_module "enable maven:3.6"
-    dnf_install "java-11-openjdk-devel maven maven-openjdk11"
-    rm -v ${CONTAINER_PATH}/etc/yum.repos.d/redhat.repo
-else
-    dnf_module "enable maven:3.6"
-    dnf_install "java-11-openjdk-devel maven maven-openjdk11"
-fi
-
-dnf_clean
+dnf_module "enable maven:3.6"
+dnf_install "java-11-openjdk-devel maven maven-openjdk11"
 dnf_clean_cache
-
-clean_files
+dnf_clean
 
 commit_container base/openjdk11-jdk:latest
-
-
-CONTAINER_UUID=$(create_container base/openjdk11-jdk:latest)
-CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
-
-buildah config --cmd '[ "/usr/sbin/init" ]' ${CONTAINER_UUID}
-buildah config --stop-signal 'SIGRTMIN+3' ${CONTAINER_UUID}
-
-commit_container openjdk11-jdk:latest
