7ed2a249c145b69db9cfcd19da00cfa891c9d773
Marcin Skarbek <git@skarbek.name>
2022-01-20 01:53:18 +0100

    refactor(rabbitmq): wip cleanup

--- rabbitmq/build.sh
+++ rabbitmq/build.sh
@@ -1,19 +1,23 @@
-#ARG registry
-#ARG repository
-#ARG tag
-#ARG release
-#
-#FROM ${registry}/${repository}:${tag}-${release}
-#
-#COPY rootfs/ /
-#
-#RUN dnf -yq install erlang rabbitmq-server\
-# && systemctl enable rabbitmq-config.service rabbitmq-server.service\
-# && dnf -yq clean all\
-# && rm -rf /var/cache/* /var/log/dnf* /var/log/hawkey* /var/log/rhsm/
-#
-#ENV RABBITMQ_USR=rabbit-user\
-#    RABBITMQ_PASS=rabbit-pass
-#
-#EXPOSE 5672/tcp
-#EXPOSE 15672/tcp
+. ../meta/common.sh
+
+CONTAINER_UUID=$(create_container systemd:latest)
+CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
+
+dnf_cache
+if [ ! -z ${IMAGE_BOOTSTRAP} ]; then
+    cp -v ./files/rabbitmq.repo ${CONTAINER_PATH}/etc/yum.repos.d/rabbitmq.repo
+    cp -v ./files/RPM-GPG-KEY-rabbitmq /etc/pki/rpm-gpg/RPM-GPG-KEY-rabbitmq
+    dnf_install "erlang rabbitmq-server"
+else
+    dnf_install "erlang rabbitmq-server"
+fi
+dnf_clean_cache
+dnf_clean
+
+buildah run -t ${CONTAINER_UUID} systemctl enable\
+ rabbitmq-config.service\
+ rabbitmq-server.service
+
+buildah config --volume /var/lib/rabbitmq ${CONTAINER_UUID}
+
+commit_container rabbitmq:latest
