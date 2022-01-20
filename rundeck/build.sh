8c6607aad458408c7037f9d791175436c61745ee
Marcin Skarbek <git@skarbek.name>
2022-01-20 04:51:25 +0100

    refactor(rundeck): wip cleanup

--- rundeck/build.sh
+++ rundeck/build.sh
@@ -1,22 +1,22 @@
-. ../meta/functions.sh
+. ../meta/common.sh
 
-CONTAINER_UUID=$(cat /proc/sys/kernel/random/uuid)
-buildah from --name=${CONTAINER_UUID} ${REGISTRY}/openjdk8-jre:latest
+CONTAINER_UUID=$(create_container openjdk11-jre:latest)
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
-copy_repo
-
-rsync_rootfs
-cp rootfs/etc/pki/rpm-gpg/RPM-GPG-KEY-rundeck /etc/pki/rpm-gpg/RPM-GPG-KEY-rundeck
-
-dnf_install "rundeck git-core"
-
+dnf_cache
+if [ ! -z ${IMAGE_BOOTSTRAP} ]; then
+    cp -v ./files/rundeck.repo ${CONTAINER_PATH}/etc/yum.repos.d/rundeck.repo
+    cp -v ./files/RPM-GPG-KEY-rundeck /etc/pki/rpm-gpg/RPM-GPG-KEY-rundeck
+    dnf_install "rundeck git-core"
+else
+    dnf_install "rundeck git-core"
+fi
+dnf_clean_cache
 dnf_clean
 
-clean_files
+rsync_rootfs
 
-#buildah run -t ${CONTAINER_UUID} systemctl enable\
-# rundeck.service
+buildah run -t ${CONTAINER_UUID} systemctl enable\
+ rundeck.service
 
-buildah commit ${CONTAINER_UUID} ${REGISTRY}/rundeck:latest
-buildah rm -a
+commit_container rundeck:latest
