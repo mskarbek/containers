ca7125aa93442bda14170b7598977cb6c4ddb563
Marcin Skarbek <git@skarbek.name>
2022-01-20 04:38:33 +0100

    refactor(rpmbuild): wip cleanup

--- rpmbuild/build.sh
+++ rpmbuild/build.sh
@@ -1,17 +1,11 @@
-. ../meta/functions.sh
+. ../meta/common.sh
 
-CONTAINER_UUID=$(cat /proc/sys/kernel/random/uuid)
-buildah from --name=${CONTAINER_UUID} ${REGISTRY}/base:latest
+CONTAINER_UUID=$(create_container base:latest)
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
 dnf_cache
-
 dnf_install "git gcc gcc-c++ clang llvm rpm-build"
-
-dnf_clean
 dnf_clean_cache
+dnf_clean
 
-clean_files
-
-buildah commit ${CONTAINER_UUID} ${REGISTRY}/build:latest
-buildah rm -a
+commit_container base/rpmbuild:latest
