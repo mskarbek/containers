090fb9c31bfb0eb1c8b747be23ae89263c35e4d8
Marcin Skarbek <git@skarbek.name>
2022-01-23 22:59:38 +0100

    feat(ansible): add ara

--- ansible/build.sh
+++ ansible/build.sh
@@ -3,8 +3,13 @@
 CONTAINER_UUID=$(create_container base/python36:latest)
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
+if [ ! -z ${IMAGE_BOOTSTRAP} ]; then
+    cp -v ./files/epel.repo ${CONTAINER_PATH}/etc/yum.repos.d/epel.repo
+    cp -v ./files/RPM-GPG-KEY-EPEL-8 /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-8
+fi
+
 dnf_cache
-dnf_install "ansible openssh-clients git-core rsync"
+dnf_install "--enablerepo=openstack-16-tools-for-rhel-8-x86_64-rpms ansible openssh-clients git-core rsync ara"
 dnf_clean_cache
 dnf_clean
 
