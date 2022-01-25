5709fcf9a0452f0a3a133ccd15af78310827949b
Marcin Skarbek <git@skarbek.name>
2022-01-25 02:27:06 +0100

    feat(meta): extend nexus.sh to deploy.sh

--- meta/deploy.sh
+++ meta/deploy.sh
@@ -4,6 +4,9 @@ ZFS_POOL="dfe93b5a-632d-44c4-98b9-7cff969b3a1d"
 REGISTRY_URL="registry.lab.skarbek.name"
 ORG_URL="lab.skarbek.name"
 DEPLOYMENT_ID=$(date +%y%m%d%H%M)
+MINIO_ROOT_PASSWORD="password123"
+NGINX_ADMIN_PASSWORD="password123"
+GITLAB_ROOT_PASSWORD="password123"
 
 podman pod create\
  --infra-image=${REGISTRY_URL}/pause:8.5-1\
@@ -31,10 +34,16 @@ podman pod create\
  --ip=10.88.0.5
 podman pod create\
  --infra-image=${REGISTRY_URL}/pause:8.5-1\
+ --infra-name=inf-gitlab-${DEPLOYMENT_ID}-infra\
+ --hostname=inf-gitlab-${DEPLOYMENT_ID}\
+ --name=inf-gitlab-${DEPLOYMENT_ID}\
+ --ip=10.88.0.6
+podman pod create\
+ --infra-image=${REGISTRY_URL}/pause:8.5-1\
  --infra-name=inf-nginx-${DEPLOYMENT_ID}-infra\
  --hostname=inf-nginx-${DEPLOYMENT_ID}\
  --name=inf-nginx-${DEPLOYMENT_ID}\
- --ip=10.88.0.6\
+ --ip=10.88.0.7\
  --publish=80:80\
  --publish=443:443
 
@@ -43,6 +52,9 @@ zfs create -o mountpoint=legacy ${ZFS_POOL}/datafs/var/lib/volumes/storage/inf-k
 zfs create -o mountpoint=legacy ${ZFS_POOL}/datafs/var/lib/volumes/storage/inf-tinyproxy-${DEPLOYMENT_ID}-app-etc
 zfs create -o mountpoint=legacy ${ZFS_POOL}/datafs/var/lib/volumes/storage/inf-minio-${DEPLOYMENT_ID}-app-lib
 zfs create -o mountpoint=legacy ${ZFS_POOL}/datafs/var/lib/volumes/storage/inf-nexus-${DEPLOYMENT_ID}-app-lib
+zfs create -o mountpoint=legacy ${ZFS_POOL}/datafs/var/lib/volumes/storage/inf-gitlab-${DEPLOYMENT_ID}-app-etc
+zfs create -o mountpoint=legacy ${ZFS_POOL}/datafs/var/lib/volumes/storage/inf-gitlab-${DEPLOYMENT_ID}-app-opt
+zfs create -o mountpoint=legacy ${ZFS_POOL}/datafs/var/lib/volumes/storage/inf-gitlab-${DEPLOYMENT_ID}-app-log
 zfs create -o mountpoint=legacy ${ZFS_POOL}/datafs/var/lib/volumes/storage/inf-nginx-${DEPLOYMENT_ID}-app-etc
 
 zfs create -o mountpoint=legacy ${ZFS_POOL}/datafs/var/lib/volumes/storage/inf-kuma-${DEPLOYMENT_ID}-mesh-etc
@@ -53,11 +65,24 @@ zfs create -o mountpoint=legacy ${ZFS_POOL}/datafs/var/lib/volumes/storage/inf-m
 zfs create -o mountpoint=legacy ${ZFS_POOL}/datafs/var/lib/volumes/storage/inf-minio-${DEPLOYMENT_ID}-mesh-log
 zfs create -o mountpoint=legacy ${ZFS_POOL}/datafs/var/lib/volumes/storage/inf-nexus-${DEPLOYMENT_ID}-mesh-etc
 zfs create -o mountpoint=legacy ${ZFS_POOL}/datafs/var/lib/volumes/storage/inf-nexus-${DEPLOYMENT_ID}-mesh-log
+zfs create -o mountpoint=legacy ${ZFS_POOL}/datafs/var/lib/volumes/storage/inf-gitlab-${DEPLOYMENT_ID}-mesh-etc
+zfs create -o mountpoint=legacy ${ZFS_POOL}/datafs/var/lib/volumes/storage/inf-gitlab-${DEPLOYMENT_ID}-mesh-log
 zfs create -o mountpoint=legacy ${ZFS_POOL}/datafs/var/lib/volumes/storage/inf-nginx-${DEPLOYMENT_ID}-mesh-etc
 zfs create -o mountpoint=legacy ${ZFS_POOL}/datafs/var/lib/volumes/storage/inf-nginx-${DEPLOYMENT_ID}-mesh-log
 
+VOLS=(inf-kuma-${DEPLOYMENT_ID}-app-etc)
+VOLS+=(inf-tinyproxy-${DEPLOYMENT_ID}-app-etc)
+VOLS+=(inf-gitlab-${DEPLOYMENT_ID}-app-etc)
+VOLS+=(inf-nginx-${DEPLOYMENT_ID}-app-etc)
+VOLS+=(inf-kuma-${DEPLOYMENT_ID}-mesh-etc)
+VOLS+=(inf-tinyproxy-${DEPLOYMENT_ID}-mesh-etc)
+VOLS+=(inf-minio-${DEPLOYMENT_ID}-mesh-etc)
+VOLS+=(inf-nexus-${DEPLOYMENT_ID}-mesh-etc)
+VOLS+=(inf-gitlab-${DEPLOYMENT_ID}-mesh-etc)
+VOLS+=(inf-nginx-${DEPLOYMENT_ID}-mesh-etc)
+
 TMP_MOUNT=$(mktemp -d)
-for VOL in {inf-kuma-${DEPLOYMENT_ID}-app-etc,inf-tinyproxy-${DEPLOYMENT_ID}-app-etc,inf-nginx-${DEPLOYMENT_ID}-app-etc,inf-kuma-${DEPLOYMENT_ID}-mesh-etc,inf-tinyproxy-${DEPLOYMENT_ID}-mesh-etc,inf-minio-${DEPLOYMENT_ID}-mesh-etc,inf-nexus-${DEPLOYMENT_ID}-mesh-etc,inf-nginx-${DEPLOYMENT_ID}-mesh-etc}; do
+for VOL in ${VOLS[@]}; do
     mkdir ${TMP_MOUNT}/${VOL}
     mount -t zfs ${ZFS_POOL}/datafs/var/lib/volumes/storage/${VOL} ${TMP_MOUNT}/${VOL}
 done
@@ -65,6 +90,8 @@ done
 cp ./files/mesh-inf.yaml ${TMP_MOUNT}/inf-kuma-${DEPLOYMENT_ID}-app-etc/mesh-inf.yaml
 cp ./files/traffic-log-inf.yaml ${TMP_MOUNT}/inf-kuma-${DEPLOYMENT_ID}-app-etc/traffic-log-inf.yaml
 cp ./files/tinyproxy.conf ${TMP_MOUNT}/inf-tinyproxy-${DEPLOYMENT_ID}-app-etc/tinyproxy.conf
+cp ./files/gitlab.rb ${TMP_MOUNT}/inf-gitlab-${DEPLOYMENT_ID}-app-etc/gitlab.rb
+sed -i "s/ORG_URL/${ORG_URL}/g" ${TMP_MOUNT}/inf-gitlab-${DEPLOYMENT_ID}-app-etc/gitlab.rb
 cp ./files/ingress.conf ${TMP_MOUNT}/inf-nginx-${DEPLOYMENT_ID}-app-etc/ingress.conf
 sed -i "s/ORG_URL/${ORG_URL}/g" ${TMP_MOUNT}/inf-nginx-${DEPLOYMENT_ID}-app-etc/ingress.conf
 cp ./files/dataplane-kuma.yaml ${TMP_MOUNT}/inf-kuma-${DEPLOYMENT_ID}-mesh-etc/dataplane.yaml
@@ -75,10 +102,12 @@ cp ./files/dataplane-minio.yaml ${TMP_MOUNT}/inf-minio-${DEPLOYMENT_ID}-mesh-etc
 sed -i "s/DEPLOYMENT_ID/${DEPLOYMENT_ID}/" ${TMP_MOUNT}/inf-minio-${DEPLOYMENT_ID}-mesh-etc/dataplane.yaml
 cp ./files/dataplane-nexus.yaml ${TMP_MOUNT}/inf-nexus-${DEPLOYMENT_ID}-mesh-etc/dataplane.yaml
 sed -i "s/DEPLOYMENT_ID/${DEPLOYMENT_ID}/" ${TMP_MOUNT}/inf-nexus-${DEPLOYMENT_ID}-mesh-etc/dataplane.yaml
+cp ./files/dataplane-gitlab.yaml ${TMP_MOUNT}/inf-gitlab-${DEPLOYMENT_ID}-mesh-etc/dataplane.yaml
+sed -i "s/DEPLOYMENT_ID/${DEPLOYMENT_ID}/" ${TMP_MOUNT}/inf-gitlab-${DEPLOYMENT_ID}-mesh-etc/dataplane.yaml
 cp ./files/dataplane-nginx.yaml ${TMP_MOUNT}/inf-nginx-${DEPLOYMENT_ID}-mesh-etc/dataplane.yaml
 sed -i "s/DEPLOYMENT_ID/${DEPLOYMENT_ID}/" ${TMP_MOUNT}/inf-nginx-${DEPLOYMENT_ID}-mesh-etc/dataplane.yaml
 
-for VOL in {inf-kuma-${DEPLOYMENT_ID}-app-etc,inf-tinyproxy-${DEPLOYMENT_ID}-app-etc,inf-nginx-${DEPLOYMENT_ID}-app-etc,inf-kuma-${DEPLOYMENT_ID}-mesh-etc,inf-tinyproxy-${DEPLOYMENT_ID}-mesh-etc,inf-minio-${DEPLOYMENT_ID}-mesh-etc,inf-nexus-${DEPLOYMENT_ID}-mesh-etc,inf-nginx-${DEPLOYMENT_ID}-mesh-etc}; do
+for VOL in ${VOLS[@]}; do
     umount ${TMP_MOUNT}/${VOL}
     rmdir ${TMP_MOUNT}/${VOL}
 done
@@ -89,6 +118,9 @@ podman volume create -o "type=zfs" -o "device=${ZFS_POOL}/datafs/var/lib/volumes
 podman volume create -o "type=zfs" -o "device=${ZFS_POOL}/datafs/var/lib/volumes/storage/inf-tinyproxy-${DEPLOYMENT_ID}-app-etc" inf-tinyproxy-${DEPLOYMENT_ID}-app-etc
 podman volume create -o "type=zfs" -o "device=${ZFS_POOL}/datafs/var/lib/volumes/storage/inf-minio-${DEPLOYMENT_ID}-app-lib" inf-minio-${DEPLOYMENT_ID}-app-lib
 podman volume create -o "type=zfs" -o "device=${ZFS_POOL}/datafs/var/lib/volumes/storage/inf-nexus-${DEPLOYMENT_ID}-app-lib" inf-nexus-${DEPLOYMENT_ID}-app-lib
+podman volume create -o "type=zfs" -o "device=${ZFS_POOL}/datafs/var/lib/volumes/storage/inf-gitlab-${DEPLOYMENT_ID}-app-etc" inf-gitlab-${DEPLOYMENT_ID}-app-etc
+podman volume create -o "type=zfs" -o "device=${ZFS_POOL}/datafs/var/lib/volumes/storage/inf-gitlab-${DEPLOYMENT_ID}-app-opt" inf-gitlab-${DEPLOYMENT_ID}-app-opt
+podman volume create -o "type=zfs" -o "device=${ZFS_POOL}/datafs/var/lib/volumes/storage/inf-gitlab-${DEPLOYMENT_ID}-app-log" inf-gitlab-${DEPLOYMENT_ID}-app-log
 podman volume create -o "type=zfs" -o "device=${ZFS_POOL}/datafs/var/lib/volumes/storage/inf-nginx-${DEPLOYMENT_ID}-app-etc" inf-nginx-${DEPLOYMENT_ID}-app-etc
 
 podman volume create -o "type=zfs" -o "device=${ZFS_POOL}/datafs/var/lib/volumes/storage/inf-kuma-${DEPLOYMENT_ID}-mesh-etc" inf-kuma-${DEPLOYMENT_ID}-mesh-etc
@@ -99,6 +131,8 @@ podman volume create -o "type=zfs" -o "device=${ZFS_POOL}/datafs/var/lib/volumes
 podman volume create -o "type=zfs" -o "device=${ZFS_POOL}/datafs/var/lib/volumes/storage/inf-minio-${DEPLOYMENT_ID}-mesh-log" inf-minio-${DEPLOYMENT_ID}-mesh-log
 podman volume create -o "type=zfs" -o "device=${ZFS_POOL}/datafs/var/lib/volumes/storage/inf-nexus-${DEPLOYMENT_ID}-mesh-etc" inf-nexus-${DEPLOYMENT_ID}-mesh-etc
 podman volume create -o "type=zfs" -o "device=${ZFS_POOL}/datafs/var/lib/volumes/storage/inf-nexus-${DEPLOYMENT_ID}-mesh-log" inf-nexus-${DEPLOYMENT_ID}-mesh-log
+podman volume create -o "type=zfs" -o "device=${ZFS_POOL}/datafs/var/lib/volumes/storage/inf-gitlab-${DEPLOYMENT_ID}-mesh-etc" inf-gitlab-${DEPLOYMENT_ID}-mesh-etc
+podman volume create -o "type=zfs" -o "device=${ZFS_POOL}/datafs/var/lib/volumes/storage/inf-gitlab-${DEPLOYMENT_ID}-mesh-log" inf-gitlab-${DEPLOYMENT_ID}-mesh-log
 podman volume create -o "type=zfs" -o "device=${ZFS_POOL}/datafs/var/lib/volumes/storage/inf-nginx-${DEPLOYMENT_ID}-mesh-etc" inf-nginx-${DEPLOYMENT_ID}-mesh-etc
 podman volume create -o "type=zfs" -o "device=${ZFS_POOL}/datafs/var/lib/volumes/storage/inf-nginx-${DEPLOYMENT_ID}-mesh-log" inf-nginx-${DEPLOYMENT_ID}-mesh-log
 
@@ -119,6 +153,7 @@ podman exec inf-kuma-${DEPLOYMENT_ID}-app kumactl generate dataplane-token --mes
 podman exec inf-kuma-${DEPLOYMENT_ID}-app kumactl generate dataplane-token --mesh=inf --name=inf-tinyproxy-${DEPLOYMENT_ID} > /run/.kuma/inf-tinyproxy-${DEPLOYMENT_ID}.token
 podman exec inf-kuma-${DEPLOYMENT_ID}-app kumactl generate dataplane-token --mesh=inf --name=inf-minio-${DEPLOYMENT_ID} > /run/.kuma/inf-minio-${DEPLOYMENT_ID}.token
 podman exec inf-kuma-${DEPLOYMENT_ID}-app kumactl generate dataplane-token --mesh=inf --name=inf-nexus-${DEPLOYMENT_ID} > /run/.kuma/inf-nexus-${DEPLOYMENT_ID}.token
+podman exec inf-kuma-${DEPLOYMENT_ID}-app kumactl generate dataplane-token --mesh=inf --name=inf-gitlab-${DEPLOYMENT_ID} > /run/.kuma/inf-gitlab-${DEPLOYMENT_ID}.token
 podman exec inf-kuma-${DEPLOYMENT_ID}-app kumactl generate dataplane-token --mesh=inf --name=inf-nginx-${DEPLOYMENT_ID} > /run/.kuma/inf-nginx-${DEPLOYMENT_ID}.token
 
 printf "KUMA_CP_ADDRESS=https://10.88.0.2:5678/\n"|podman secret create kuma-config -
@@ -161,6 +196,15 @@ podman run\
  ${REGISTRY_URL}/bootstrap/kuma-dp:latest
 podman run\
  --detach\
+ --pod=inf-gitlab-${DEPLOYMENT_ID}\
+ --name=inf-gitlab-${DEPLOYMENT_ID}-mesh\
+ --volume=inf-gitlab-${DEPLOYMENT_ID}-mesh-etc:/etc/kuma:z\
+ --volume=inf-gitlab-${DEPLOYMENT_ID}-mesh-log:/var/log/kuma:z\
+ --volume=/run/.kuma/inf-gitlab-${DEPLOYMENT_ID}.token:/run/secrets/kuma-token:z\
+ --secret=kuma-config,type=mount\
+ ${REGISTRY_URL}/bootstrap/kuma-dp:latest
+podman run\
+ --detach\
  --pod=inf-nginx-${DEPLOYMENT_ID}\
  --name=inf-nginx-${DEPLOYMENT_ID}-mesh\
  --volume=inf-nginx-${DEPLOYMENT_ID}-mesh-etc:/etc/kuma:z\
@@ -169,27 +213,39 @@ podman run\
  --secret=kuma-config,type=mount\
  ${REGISTRY_URL}/bootstrap/kuma-dp:latest
 
+printf "MINIO_ROOT_PASSWORD=${MINIO_ROOT_PASSWORD}\n"|podman secret create minio-root-password -
+printf "NEXUS_PASSWORD=${NGINX_ADMIN_PASSWORD}\n"|podman secret create nexus-password -
+printf "GITLAB_ROOT_PASSWORD=${GITLAB_ROOT_PASSWORD}\n"|podman secret create gitlab-password -
+
 podman run\
  --detach\
  --pod=inf-tinyproxy-${DEPLOYMENT_ID}\
  --name=inf-tinyproxy-${DEPLOYMENT_ID}-app\
  --volume=inf-tinyproxy-${DEPLOYMENT_ID}-app-etc:/etc/tinyproxy:z\
  ${REGISTRY_URL}/bootstrap/tinyproxy:latest
-
 podman run\
  --detach\
  --pod=inf-minio-${DEPLOYMENT_ID}\
  --name=inf-minio-${DEPLOYMENT_ID}-app\
  --volume=inf-minio-${DEPLOYMENT_ID}-app-lib:/var/lib/minio:z\
+ --secret=minio-root-password,type=mount\
  ${REGISTRY_URL}/bootstrap/minio:latest
-
 podman run\
  --detach\
  --pod=inf-nexus-${DEPLOYMENT_ID}\
  --name=inf-nexus-${DEPLOYMENT_ID}-app\
  --volume=inf-nexus-${DEPLOYMENT_ID}-app-lib:/var/lib/sonatype-work:z\
+ --secret=nexus-password,type=mount\
  ${REGISTRY_URL}/bootstrap/nexus:latest
-
+podman run\
+ --detach\
+ --pod=inf-gitlab-${DEPLOYMENT_ID}\
+ --name=inf-gitlab-${DEPLOYMENT_ID}-app\
+ --volume=inf-gitlab-${DEPLOYMENT_ID}-app-etc:/etc/gitlab:z\
+ --volume=inf-gitlab-${DEPLOYMENT_ID}-app-opt:/var/opt/gitlab:z\
+ --volume=inf-gitlab-${DEPLOYMENT_ID}-app-log:/var/log/gitlab:z\
+ --secret=gitlab-password,type=mount\
+ ${REGISTRY_URL}/bootstrap/gitlab:latest
 podman run\
  --detach\
  --pod=inf-nginx-${DEPLOYMENT_ID}\
