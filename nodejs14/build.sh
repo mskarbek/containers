42ffe790190014bce36c2da532a9823dfad2b4dc
Marcin Skarbek <git@skarbek.name>
2022-01-27 12:00:40 +0100

    chore: add deps

--- nodejs14/build.sh
+++ nodejs14/build.sh
@@ -5,7 +5,7 @@ CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
 dnf_cache
 dnf_module "enable nodejs:14"
-dnf_install "nodejs npm"
+dnf_install "nodejs npm tar unzip git-core gzip"
 dnf_clean_cache
 dnf_clean
 
