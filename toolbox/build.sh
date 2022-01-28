56019516160d5b5eeb2592e6d0095b0fc4971f57
Marcin Skarbek <git@skarbek.name>
2022-01-28 17:24:26 +0100

    feat(toolbox): add jq

--- toolbox/build.sh
+++ toolbox/build.sh
@@ -4,7 +4,7 @@ CONTAINER_UUID=$(create_container base:latest)
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
 dnf_cache
-dnf_install "curl vi nano telnet hostname iputils iproute mtr tmux lsof bind-utils tar rsync"
+dnf_install "curl vi nano telnet hostname iputils iproute mtr tmux lsof bind-utils tar rsync jq"
 dnf_clean_cache
 dnf_clean
 
