acb1dd1fdbeee83f6a8899965931edbd819593b9
Marcin Skarbek <git@skarbek.name>
2022-01-29 20:00:30 +0100

    fix(zigbee2mqtt): build script paths

--- zigbee2mqtt/build.sh
+++ zigbee2mqtt/build.sh
@@ -1,24 +1,24 @@
 . ../meta/common.sh
 . ./files/VERSIONS
 
-CONTAINER_UUID=$(create_container base/nodejs16-devel:latest)
-CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
+BUILD_UUID=$(create_container base/nodejs16-devel:latest)
+BUILD_PATH=$(buildah mount ${BUILD_UUID})
 
-TMP_DIR=$(buildah run ${CONTAINER_UUID} mktemp -d)
+TMP_DIR=$(buildah run ${BUILD_UUID} mktemp -d)
 if [ -f ./files/${ZIGBEE2MQTT_VERSION}.tar.gz ]; then
-    tar xvf ./files/${ZIGBEE2MQTT_VERSION}.tar.gz -C ${CONTAINER_PATH}${TMP_DIR}
+    tar xvf ./files/${ZIGBEE2MQTT_VERSION}.tar.gz -C ${BUILD_PATH}${TMP_DIR}
 else
-    pushd ${CONTAINER_PATH}${TMP_DIR}
+    pushd ${BUILD_PATH}${TMP_DIR}
         curl -L https://github.com/Koenkk/zigbee2mqtt/archive/refs/tags/${ZIGBEE2MQTT_VERSION}.tar.gz|tar xvz
     popd
 fi
 
-buildah run -t --workingdir=${TMP_DIR} ${CONTAINER_UUID} npm ci
+buildah run -t --workingdir=${TMP_DIR}/zigbee2mqtt-${ZIGBEE2MQTT_VERSION} ${BUILD_UUID} npm ci
 
 CONTAINER_UUID=$(create_container base/nodejs16:latest)
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
-mv -v ${TMP_DIR}/zigbee2mqtt-${ZIGBEE2MQTT_VERSION} ${CONTAINER_PATH}/usr/local/lib/zigbee2mqtt
+mv -v ${BUILD_PATH}${TMP_DIR}/zigbee2mqtt-${ZIGBEE2MQTT_VERSION} ${CONTAINER_PATH}/usr/local/lib/zigbee2mqtt
 rsync_rootfs
 
 buildah run -t ${CONTAINER_UUID} systemctl enable\
