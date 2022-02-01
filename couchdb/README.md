364ea5548dceb59bcd66f7a1ecec3b7f01d8bfd8
Marcin Skarbek <git@skarbek.name>
2022-02-01 22:15:30 +0100

    refactor(chouchdb): change image build, add services

---
+++ couchdb/README.md
@@ -0,0 +1,9 @@
+# couchdb
+
+## WIP
+
+`/opt/couchdb/etc/local.d/couchdb.ini`:
+```ini
+[couchdb]
+single_node=true
+```
--- couchdb/build.sh
+++ couchdb/build.sh
@@ -3,20 +3,22 @@
 CONTAINER_UUID=$(create_container systemd:latest)
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
-dnf_cache
 if [ ! -z ${IMAGE_BOOTSTRAP} ]; then
     cp -v ./files/couchdb.repo ${CONTAINER_PATH}/etc/yum.repos.d/couchdb.repo
     cp -v ./files/RPM-GPG-KEY-couchdb /etc/pki/rpm-gpg/RPM-GPG-KEY-couchdb
-    dnf_install "couchdb"
-else
-    dnf_install "couchdb"
 fi
+
+dnf_cache
+dnf_install "couchdb"
 dnf_clean_cache
 dnf_clean
 
+rsync_rootfs
+
 buildah run -t ${CONTAINER_UUID} systemctl enable\
  couchdb.service
- 
+
+buildah config --volume /opt/couchdb/etc/local.d ${CONTAINER_UUID}
 buildah config --volume /var/lib/couchdb ${CONTAINER_UUID}
 buildah config --volume /var/log/couchdb ${CONTAINER_UUID}
 
