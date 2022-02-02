7508ca009cdd5b107744375eeef00a2af537db01
Marcin Skarbek <git@skarbek.name>
2022-02-02 03:37:41 +0100

    refactor(consul): move config

--- consul/build.sh
+++ consul/build.sh
@@ -5,16 +5,14 @@ CONTAINER_UUID=$(create_container systemd:latest)
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
 TMP_DIR=$(mktemp -d)
+pushd ${TMP_DIR}
 if [ -f "./files/consul_${CONSUL_VERSION}_linux_amd64.zip" ]; then
-    pushd ${TMP_DIR}
-        unzip consul_${CONSUL_VERSION}_linux_amd64.zip
-    popd
+    unzip consul_${CONSUL_VERSION}_linux_amd64.zip
 else
-    pushd ${TMP_DIR}
-        curl -L -O https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_linux_amd64.zip
-        unzip consul_${CONSUL_VERSION}_linux_amd64.zip
-    popd
+    curl -L -O https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_linux_amd64.zip
+    unzip consul_${CONSUL_VERSION}_linux_amd64.zip
 fi
+popd
 mv -v ${TMP_DIR}/consul ${CONTAINER_PATH}/usr/local/bin/consul
 chmod -v 0755 ${CONTAINER_PATH}/usr/local/bin/consul
 rm -rf ${TMP_DIR}
@@ -24,6 +22,7 @@ rsync_rootfs
 buildah run -t ${CONTAINER_UUID} systemctl enable\
  consul.service
 
+buildah config --volume /etc/consul.d ${CONTAINER_UUID}
 buildah config --volume /var/lib/consul ${CONTAINER_UUID}
 
 commit_container consul:latest
