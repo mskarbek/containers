7924fb859a46e5214fba8cbccb0f8c5d59f52039
Marcin Skarbek <git@skarbek.name>
2022-02-03 16:33:50 +0100

    feat(toolbox): add openssh-clients

--- toolbox/build.sh
+++ toolbox/build.sh
@@ -1,10 +1,17 @@
 . ../meta/common.sh
 
+TOOLS="curl vi nano telnet hostname iputils iproute mtr tmux lsof bind-utils tar rsync jq htop openssh-clients"
+
 CONTAINER_UUID=$(create_container base:latest)
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
+if [ ! -z ${IMAGE_BOOTSTRAP} ]; then
+    cp -v ./files/{epel.repo,centos-hyperscale.repo} ${CONTAINER_PATH}/etc/yum.repos.d/
+    cp -v ./files/{RPM-GPG-KEY-EPEL-8,RPM-GPG-KEY-CentOS-SIG-HyperScale} /etc/pki/rpm-gpg/
+fi
+
 dnf_cache
-dnf_install "curl vi nano telnet hostname iputils iproute mtr tmux lsof bind-utils tar rsync jq"
+dnf_install ${TOOLS}
 dnf_clean_cache
 dnf_clean
 
@@ -14,8 +21,13 @@ commit_container base/toolbox:latest
 CONTAINER_UUID=$(create_container openssh:latest)
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
+if [ ! -z ${IMAGE_BOOTSTRAP} ]; then
+    cp -v ./files/{epel.repo,centos-hyperscale.repo} ${CONTAINER_PATH}/etc/yum.repos.d/
+    cp -v ./files/{RPM-GPG-KEY-EPEL-8,RPM-GPG-KEY-CentOS-SIG-HyperScale} /etc/pki/rpm-gpg/
+fi
+
 dnf_cache
-dnf_install "curl vi nano telnet hostname iputils iproute mtr tmux lsof bind-utils tar rsync"
+dnf_install ${TOOLS}
 dnf_clean_cache
 dnf_clean
 
