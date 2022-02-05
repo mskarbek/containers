65af7c33539cc30f301aa788706c11920b50286c
Marcin Skarbek <git@skarbek.name>
2022-02-05 19:08:33 +0100

    fix(base): bring back systemd form hyperscale

--- base/build.sh
+++ base/build.sh
@@ -4,8 +4,8 @@ CONTAINER_UUID=$(create_container micro:latest)
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
 if [ ! -z ${IMAGE_BOOTSTRAP} ]; then
-    cp -v ./files/epel.repo ${CONTAINER_PATH}/etc/yum.repos.d/epel.repo
-    cp -v ./files/RPM-GPG-KEY-EPEL-8 /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-8
+    cp -v ./files/{epel.repo,centos-hyperscale.repo} ${CONTAINER_PATH}/etc/yum.repos.d/
+    cp -v ./files/{RPM-GPG-KEY-EPEL-8,RPM-GPG-KEY-CentOS-SIG-HyperScale} /etc/pki/rpm-gpg/
 fi
 
 dnf_cache
