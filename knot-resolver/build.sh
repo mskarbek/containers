aa2ff602bb44b53ea973fd90d29bb346f6a511e8
Marcin Skarbek <git@skarbek.name>
2022-02-06 10:59:35 +0100

    feat(knot-resolver): add config volume

--- knot-resolver/build.sh
+++ knot-resolver/build.sh
@@ -8,7 +8,13 @@ dnf_install "knot-resolver knot-resolver-module-dnstap knot-resolver-module-http
 dnf_clean_cache
 dnf_clean
 
+mkdir -vp ${CONTAINER_PATH}/usr/share/knot-resolver
+mv -v ${CONTAINER_PATH}/etc/knot-resolver/* ${CONTAINER_PATH}/usr/share/knot-resolver
+rsync_rootfs
+
 buildah run -t ${CONTAINER_UUID} systemctl enable\
  kresd@1.service
+
+buildah config --volume /etc/knot-resolver ${CONTAINER_UUID}
 
 commit_container knot-resolver:latest
