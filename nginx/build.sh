0ab7ba63e21e7f8cd49e2829cacb956818930aad
Marcin Skarbek <git@skarbek.name>
2022-02-06 08:44:14 +0100

    fix(nginx): disable epel-modular

--- nginx/build.sh
+++ nginx/build.sh
@@ -6,7 +6,7 @@ CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 dnf_cache
 dnf_module "disable php"
 dnf_module "--disablerepo=epel-modular enable nginx:1.20"
-dnf_install "nginx nginx-mod-mail nginx-mod-stream"
+dnf_install "--disablerepo=epel-modular nginx nginx-mod-mail nginx-mod-stream"
 dnf_clean_cache
 dnf_clean
 
