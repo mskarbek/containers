f754064dcfec28474940c3646486737e4732c654
Marcin Skarbek <git@skarbek.name>
2022-02-07 19:04:23 +0100

    feat(pgadmin4): add new image

--- pgadmin4/build.sh
+++ pgadmin4/build.sh
@@ -3,20 +3,35 @@
 CONTAINER_UUID=$(create_container systemd:latest)
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
+if [ ! -z ${IMAGE_BOOTSTRAP} ]; then
+    cp -v ./files/{pgdg-redhat,pgadmin4}.repo ${CONTAINER_PATH}/etc/yum.repos.d/
+    cp -v ./files/RPM-GPG-KEY-{PGDG,PGADMIN} /etc/pki/rpm-gpg/
+fi
+
 dnf_cache
 dnf_module "disable postgresql"
-if [ ! -z ${IMAGE_BOOTSTRAP} ]; then
-    cp -v ./files/pgdg-redhat.repo ${CONTAINER_PATH}/etc/yum.repos.d/pgdg-redhat.repo
-    cp -v ./files/pgadmin4.repo ${CONTAINER_PATH}/etc/yum.repos.d/pgadmin4.repo
-    cp -v ./files/RPM-GPG-KEY-PGDG /etc/pki/rpm-gpg/RPM-GPG-KEY-PGDG
-    cp -v ./files/RPM-GPG-KEY-PGADMIN /etc/pki/rpm-gpg/RPM-GPG-KEY-PGADMIN
-    dnf_install "libstdc++ pgadmin4-web"
-else
-    dnf_install "libstdc++ pgadmin4-web"
-fi
+dnf_install "libstdc++ pgadmin4-server postgresql12 postgresql13 postgresql14"
 dnf_clean_cache
 dnf_clean
 
-#buildah run -t ${CONTAINER_UUID} /usr/pgadmin4/bin/setup-web.sh
+buildah run -t ${CONTAINER_UUID} bash -c "source /usr/pgadmin4/venv/bin/activate; pip3 install gunicorn"
+
+cat << EOF > ${CONTAINER_PATH}/usr/pgadmin4/web/config_distro.py
+DEFAULT_BINARY_PATHS = {
+        'pg': '/usr/pgsql-14/bin',
+        'pg-14': '/usr/pgsql-14/bin',
+        'pg-13': '/usr/pgsql-13/bin',
+        'pg-12': '/usr/pgsql-12/bin'
+}
+EOF
+
+rsync_rootfs
+
+buildah run -t ${CONTAINER_UUID} systemctl enable\
+ pgadmin4.service
+
+buildah config --volume /etc/pgadmin ${CONTAINER_UUID}
+buildah config --volume /var/lib/pgadmin ${CONTAINER_UUID}
+buildah config --volume /var/log/pgadmin ${CONTAINER_UUID}
 
 commit_container pgadmin4:latest
