8a862e453b59ad6bc01713b58d2e7e4703e69b94
Marcin Skarbek <git@skarbek.name>
2022-02-07 15:28:45 +0100

    chore: drop few unused images

--- kafka-all-in-one/rootfs/usr/local/bin/kafka-update-ip.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ -f /opt/kafka/config/.lock ]]; then
-    exit 0
-fi
-
-IP=$(cat /proc/net/fib_trie|awk '/32 host/ {print ip} {ip=$2}'|sort|uniq|grep -v 127)
-sed -i "s/#listeners=PLAINTEXT:\/\/:9092/listeners=PLAINTEXT:\/\/${IP}:9092/" /opt/kafka/config/server.properties
-touch /opt/kafka/config/.lock
