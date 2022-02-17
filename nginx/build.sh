e86303e7ed73c80bc74ce71daa799b14ef6ef9c5
Marcin Skarbek <git@skarbek.name>
2022-02-17 13:17:44 +0100

    feat(nginx): add mod_stream config support, add config reload on config change

--- nginx/build.sh
+++ nginx/build.sh
@@ -10,13 +10,20 @@ dnf_install "--disablerepo=epel-modular nginx nginx-mod-mail nginx-mod-stream"
 dnf_clean_cache
 dnf_clean
 
+rm -vf ${CONTAINER_PATH}/etc/nginx/nginx.conf
 rm -vf ${CONTAINER_PATH}/usr/share/nginx/html/index.html
 touch ${CONTAINER_PATH}/usr/share/nginx/html/index.html
 
+rsync_rootfs
+
 buildah run -t ${CONTAINER_UUID} systemctl enable\
+ etc-nginx-conf.d.path\
+ etc-nginx-stream.d.path\
  nginx.service
 
 buildah config --volume /etc/nginx/conf.d ${CONTAINER_UUID}
+buildah config --volume /etc/nginx/stream.d ${CONTAINER_UUID}
+buildah config --volume /etc/pki/tls/private ${CONTAINER_UUID}
 buildah config --volume /var/log/nginx ${CONTAINER_UUID}
 
 commit_container nginx:latest
