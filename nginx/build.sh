9f8b42c0c2053254e3e7ba1d50f03fdb8f16fcd5
Marcin Skarbek <git@skarbek.name>
2022-06-08 13:15:22 +0200

    feat(nginx): update to rhel9

--- nginx/build.sh
+++ nginx/build.sh
@@ -4,9 +4,7 @@ CONTAINER_UUID=$(create_container systemd:latest)
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
 dnf_cache
-dnf_module "disable php"
-dnf_module "--disablerepo=epel-modular enable nginx:1.20"
-dnf_install "--disablerepo=epel-modular nginx nginx-mod-mail nginx-mod-stream"
+dnf_install "nginx nginx-mod-mail nginx-mod-stream"
 dnf_clean_cache
 dnf_clean
 
