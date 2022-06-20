1e26237c27a0607e308f935e6e83d6d780ab68ac
Marcin Skarbek <git@skarbek.name>
2022-06-20 10:35:02 +0200

    feat(ansible): update to rhel9

--- ansible/build.sh
+++ ansible/build.sh
@@ -1,22 +1,14 @@
 . ../meta/common.sh
 
-CONTAINER_UUID=$(create_container base/python36:latest)
+CONTAINER_UUID=$(create_container base/python39:latest)
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
-if [ ${BASE_OS} = "el8" ]; then
-    ENABLE_REPO="openstack-16-tools-for-rhel-8-x86_64-rpms"
-elif [ ${BASE_OS} = "c8s" ]; then
-    cp -v ./files/centos-{sig-ansible,openstack-xena,ceph-pacific}.repo ${CONTAINER_PATH}/etc/yum.repos.d/
-    cp -v ./RPM-GPG-KEY-CentOS-SIG-{Cloud,Storage,ConfigManagement} /etc/pki/rpm-gpg/
-    ENABLE_REPO="centos-openstack-xena --enablerepo=centos-ceph-pacific"
-else
-    printf "ERROR: Missing or incorrect BASE_OS variable." >&2
-    exit 1
-fi
-
 dnf_cache
-dnf_install "--enablerepo=${ENABLE_REPO} ansible openssh-clients git-core rsync ara https://download.copr.fedorainfracloud.org/results/mskarbek/epel-ext/epel-8-x86_64/03258221-python-consul/python3-consul-1.1.0-1.el8.noarch.rpm"
+dnf_install "ansible-core openssh-clients git-core rsync python3-requests python3-certifi python3-idna python3-charset-normalizer python3-wcwidth python3-pbr python3-attrs python3-urllib3 python3-prettytable"
 dnf_clean_cache
 dnf_clean
+
+buildah run -t ${CONTAINER_UUID} pip3 install ara python-consul
+rm -rvf ${CONTAINER_PATH}/root/.cache
 
 commit_container base/ansible:latest
--- python39/build.sh
+++ python39/build.sh
@@ -4,7 +4,7 @@ CONTAINER_UUID=$(create_container base:latest)
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
 dnf_cache
-dnf_install "python39 python39-pip python39-pip-wheel"
+dnf_install "python3 python3-pip python3-setuptools python3-wheel python3-pip-wheel python3-setuptools-wheel"
 dnf_clean_cache
 dnf_clean
 
