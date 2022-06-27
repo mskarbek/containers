7ff81e7a18cc7ef093cc0107301ff8d3542b037b
Marcin Skarbek <git@skarbek.name>
2022-06-27 06:48:22 +0200

    feat(minio): update for new ci

--- minio/build.sh
+++ minio/build.sh
@@ -1,23 +1,23 @@
 . ../meta/common.sh
 . ./files/VERSIONS
 
-CONTAINER_UUID=$(create_container systemd:latest)
+if [ -z ${1} ]; then
+    CONTAINER_UUID=$(create_container systemd latest)
+else
+    CONTAINER_UUID=$(create_container systemd ${1})
+fi
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
-if [ -f ./files/minio ] && [ -f ./files/mcli ]; then
-    cp -v ./files/{minio,mcli} ${CONTAINER_PATH}/usr/local/bin/
-else
-    curl -L -o ${CONTAINER_PATH}/usr/local/bin/minio https://dl.min.io/server/minio/release/linux-amd64/archive/minio.${MINIO_VERSION}
-    curl -L -o ${CONTAINER_PATH}/usr/local/bin/mcli https://dl.min.io/client/mc/release/linux-amd64/archive/mc.${MCLI_VERSION}
-fi
+curl -L -o ${CONTAINER_PATH}/usr/local/bin/minio https://dl.min.io/server/minio/release/linux-amd64/archive/minio.${MINIO_VERSION}
+curl -L -o ${CONTAINER_PATH}/usr/local/bin/mcli https://dl.min.io/client/mc/release/linux-amd64/archive/mc.${MCLI_VERSION}
 chmod -v 0755 ${CONTAINER_PATH}/usr/local/bin/{minio,mcli}
 
 rsync_rootfs
 
-buildah run -t ${CONTAINER_UUID} systemctl enable\
+buildah run --network none ${CONTAINER_UUID} systemctl enable\
  minio.service
 
 buildah config --volume /etc/minio ${CONTAINER_UUID}
 buildah config --volume /var/lib/minio ${CONTAINER_UUID}
 
-commit_container minio:latest
+commit_container minio ${IMAGE_TAG}
