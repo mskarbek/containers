3767f9e8de5f1d9cf5f5fadfe12e9a8a179449e1
Marcin Skarbek <git@skarbek.name>
2022-06-27 14:26:12 +0200

    feat(tinyproxy): update for new ci

--- tinyproxy/build.sh
+++ tinyproxy/build.sh
@@ -1,12 +1,14 @@
 . ../meta/common.sh
 
-CONTAINER_UUID=$(create_container systemd:latest)
+if [ -z ${1} ]; then
+    CONTAINER_UUID=$(create_container systemd latest)
+else
+    CONTAINER_UUID=$(create_container systemd ${1})
+fi
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
 dnf_cache
-if [ -f ./files/tinyproxy-1.11.0-1.el9.x86_64.rpm ] && [ ! -z ${IMAGE_BOOTSTRAP} ]; then
-    dnf_install "./files/tinyproxy-1.11.0-1.el9.x86_64.rpm"
-elif [ ! -z ${IMAGE_BOOTSTRAP} ]; then
+if [ ! -z ${IMAGE_BOOTSTRAP} ]; then
     dnf_install "https://download.copr.fedorainfracloud.org/results/mskarbek/tinyproxy/epel-9-x86_64/04426720-tinyproxy/tinyproxy-1.11.0-1.el9.x86_64.rpm"
 else
     dnf_install "tinyproxy"
@@ -14,9 +16,9 @@ fi
 dnf_clean_cache
 dnf_clean
 
-buildah run -t ${CONTAINER_UUID} systemctl enable\
+buildah run --network none ${CONTAINER_UUID} systemctl enable\
  tinyproxy.service
 
 buildah config --volume /etc/tinyproxy ${CONTAINER_UUID}
 
-commit_container tinyproxy:latest
+commit_container tinyproxy ${IMAGE_TAG}
