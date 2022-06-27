d5d224f076bcb7871afa7a6ec02b38dc4e21ab65
Marcin Skarbek <git@skarbek.name>
2022-06-27 06:53:50 +0200

    feat(minio-consul): update for new ci

--- minio-console/build.sh
+++ minio-console/build.sh
@@ -1,21 +1,21 @@
 . ../meta/common.sh
 . ./files/VERSIONS
 
-CONTAINER_UUID=$(create_container systemd:latest)
+if [ -z ${1} ]; then
+    CONTAINER_UUID=$(create_container systemd latest)
+else
+    CONTAINER_UUID=$(create_container systemd ${1})
+fi
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
-if [ -f ./files/minio-console ]; then
-    cp -v ./files/minio-console ${CONTAINER_PATH}/usr/local/bin/
-else
-    curl -L -o ${CONTAINER_PATH}/usr/local/bin/minio-console https://github.com/minio/console/releases/download/v${MINIO_CONSOLE_VERSION}/console-linux-amd64
-fi
+curl -L -o ${CONTAINER_PATH}/usr/local/bin/minio-console https://github.com/minio/console/releases/download/v${MINIO_CONSOLE_VERSION}/console-linux-amd64
 chmod -v 0755 ${CONTAINER_PATH}/usr/local/bin/minio-console
 
 rsync_rootfs
 
-buildah run -t ${CONTAINER_UUID} systemctl enable\
+buildah run --network none ${CONTAINER_UUID} systemctl enable\
  minio-console.service
 
 buildah config --volume /etc/minio-console ${CONTAINER_UUID}
 
-commit_container minio-console:latest
+commit_container minio-console ${IMAGE_TAG}
