25865e6ab567cd50f9657d596884013e9bcbf7c4
Marcin Skarbek <git@skarbek.name>
2022-06-27 08:02:48 +0200

    feat(gitlab-runner): update for new ci

--- gitlab-runner/build.sh
+++ gitlab-runner/build.sh
@@ -1,8 +1,24 @@
 . ../meta/common.sh
 . ./files/VERSIONS
 
-for IMAGE in {podman,podman-zfs,buildah,buildah-zfs}; do
-    CONTAINER_UUID=$(create_container ${IMAGE}:latest)
+if [ ! -z ${FROM_IMAGE} ]; then
+    if [ ${FROM_IMAGE} == "buildah" ]; then
+        IMAGES="buildah buildah-zfs"
+    elif [ ${FROM_IMAGE} == "podman" ]; then
+        IMAGES="podman podman-zfs"
+    else
+        IMAGES="buildah buildah-zfs podman podman-zfs"
+    fi
+else
+    IMAGES="buildah buildah-zfs podman podman-zfs"
+fi
+
+for IMAGE in ${IMAGES}; do
+    if [ -z ${1} ]; then
+        CONTAINER_UUID=$(create_container ${IMAGE} latest)
+    else
+        CONTAINER_UUID=$(create_container ${IMAGE} ${1})
+    fi
     CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
     dnf_cache
@@ -10,20 +26,16 @@ for IMAGE in {podman,podman-zfs,buildah,buildah-zfs}; do
     dnf_clean_cache
     dnf_clean
 
-    if [ -f "./files/gitlab-runner" ]; then
-        mv -v ./files/gitlab-runner ${CONTAINER_PATH}/usr/local/bin/
-    else
-        curl -L -o ${CONTAINER_PATH}/usr/local/bin/gitlab-runner https://gitlab-runner-downloads.s3.amazonaws.com/v${GITLAB_RUNNER_VERSION}/binaries/gitlab-runner-linux-amd64
-    fi
-    chmod -v 0755 ${CONTAINER_PATH}/usr/local/bin/*
+    curl -L -o ${CONTAINER_PATH}/usr/local/bin/gitlab-runner https://gitlab-runner-downloads.s3.amazonaws.com/v${GITLAB_RUNNER_VERSION}/binaries/gitlab-runner-linux-amd64
+    chmod -v 0755 ${CONTAINER_PATH}/usr/local/bin/gitlab-runner
 
     rsync_rootfs
 
-    buildah run -t ${CONTAINER_UUID} systemctl enable\
+    buildah run --network none ${CONTAINER_UUID} systemctl enable\
      gitlab-runner.service
 
     buildah config --volume /etc/gitlab-runner ${CONTAINER_UUID}
     buildah config --volume /var/lib/gitlab-runner ${CONTAINER_UUID}
 
-    commit_container gitlab-runner-${IMAGE}:latest
+    commit_container gitlab-runner-${IMAGE} ${IMAGE_TAG}
 done
