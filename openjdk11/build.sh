b719bbd33f4b307de1efc76f1cc602d9d7b9e251
Marcin Skarbek <git@skarbek.name>
2022-06-27 05:42:57 +0200

    feat(openjdk11): update for new ci

--- openjdk11/build.sh
+++ openjdk11/build.sh
@@ -1,6 +1,10 @@
 . ../meta/common.sh
 
-CONTAINER_UUID=$(create_container base:latest)
+if [ -z ${1} ]; then
+    CONTAINER_UUID=$(create_container base latest)
+else
+    CONTAINER_UUID=$(create_container base ${1})
+fi
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
 dnf_cache
@@ -9,20 +13,20 @@ dnf_install "java-11-openjdk-headless apr"
 dnf_clean_cache
 dnf_clean
 
-commit_container base/openjdk11-jre:latest
+commit_container base/openjdk11-jre ${IMAGE_TAG}
 
 
-CONTAINER_UUID=$(create_container base/openjdk11-jre:latest)
+CONTAINER_UUID=$(create_container base/openjdk11-jre ${IMAGE_TAG})
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
 buildah config --cmd '[ "/usr/sbin/init" ]' ${CONTAINER_UUID}
 buildah config --stop-signal 'SIGRTMIN+3' ${CONTAINER_UUID}
 buildah config --volume /var/log/journal ${CONTAINER_UUID}
 
-commit_container openjdk11-jre:latest
+commit_container openjdk11-jre ${IMAGE_TAG}
 
 
-CONTAINER_UUID=$(create_container base/openjdk11-jre:latest)
+CONTAINER_UUID=$(create_container base/openjdk11-jre ${IMAGE_TAG})
 CONTAINER_PATH=$(buildah mount ${CONTAINER_UUID})
 
 dnf_cache
@@ -30,4 +34,4 @@ dnf_install "java-11-openjdk-devel maven maven-openjdk11"
 dnf_clean_cache
 dnf_clean
 
-commit_container base/openjdk11-jdk:latest
+commit_container base/openjdk11-jdk ${IMAGE_TAG}
