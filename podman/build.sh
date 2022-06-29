597547c2409bfb7afda1cc79613247bc4114eb8d
Marcin Skarbek <git@skarbek.name>
2022-06-29 11:18:28 +0200

    fix: add missing dep

--- podman/build.sh
+++ podman/build.sh
@@ -7,7 +7,7 @@ source ./files/VERSIONS
 container_create systemd ${1}
 
 dnf_cache
-dnf_install "podman skopeo fuse-overlayfs netavark iputils iproute iptables-nft"
+dnf_install "podman skopeo fuse-overlayfs netavark aardvark-dns iputils iproute iptables-nft"
 dnf_cache_clean
 dnf_clean
 
