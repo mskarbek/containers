70bf72a124550d21d2618e9f79583d4146a02e7d
Marcin Skarbek <git@skarbek.name>
2022-06-29 10:59:12 +0200

    feat(meta): mirror script

---
+++ meta/mirror.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -u
+
+TMP_DIR=$(mktemp -d -p /tmp mirror.XXX)
+pushd ${TMP_DIR}
+    MIRROR_DIR=$(mktemp -d -p ${TMP_DIR} containers.XXX)
+    git clone --quiet https://github.com/mskarbek/containers.git ${MIRROR_DIR}
+    for IMAGE in $(ls -1 --hide=README.md --hide=meta ${MIRROR_DIR}); do
+        REPO_DIR=$(mktemp -d -p ${TMP_DIR} ${IMAGE}.XXX)
+        git clone --quiet git@git.lab.skarbek.name:containers/$IMAGE.git ${REPO_DIR}
+        rsync -a --delete --exclude=".git" ${MIRROR_DIR}/${IMAGE}/ ${REPO_DIR}/
+        pushd ${REPO_DIR}
+            git add .
+            git diff-index --cached --quiet HEAD --
+            if [ $? -ne 0 ]; then
+                git commit -m 'chore: mirror update'
+                git push -o ci.skip
+            fi
+        popd
+    done
+popd
+rm -rf ${TMP_DIR}
