949b71e4bd06c9e496a13e1344248f9cd6de2d58
Marcin Skarbek <git@skarbek.name>
2022-07-18 04:12:55 +0200

    feat(python3): add pip.conf

--- python3/build.sh
+++ python3/build.sh
@@ -10,7 +10,20 @@ dnf_install "python3 python3-pip python3-setuptools python3-wheel python3-pip-wh
 dnf_cache_clean
 dnf_clean
 
-rsync_rootfs
+if [ ${IMAGE_BOOTSTRAP} != "true" ]; then
+    cat << EOF > ${CONTAINER_PATH}/etc/pip.conf
+[global]
+trusted-host = ${REPOSITORY_PIP_TRUSTED_HOST}
+index = ${REPOSITORY_PIP_INDEX}
+index-url = ${REPOSITORY_PIP_INDEX_URL}
+EOF
+    cat << EOF > ${CONTAINER_PATH}/root/.netrc
+machine ${REPOSITORY_PIP_TRUSTED_HOST}
+    login ${REPOSITORY_USERNAME}
+    password ${REPOSITORY_PASSWORD}
+EOF
+    chmod -v 0600 ${CONTAINER_PATH}/root/.netrc
+fi
 
 container_commit base/python3 ${IMAGE_TAG}
 
