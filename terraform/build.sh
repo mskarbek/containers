26d2a0087ed1025ea2b4fe0a3d4ef69172458bf8
Marcin Skarbek <git@skarbek.name>
2022-07-18 04:57:57 +0200

    feat(terraform): add local source

--- terraform/build.sh
+++ terraform/build.sh
@@ -2,14 +2,19 @@
 set -eu
 
 source ../meta/common.sh
-source ./files/VERSIONS
 
 container_create base ${1}
 
+VERSION=$(jq -r .[0].version ./files/versions.json)
+PWD_DIR=$(pwd)
 TMP_DIR=$(mktemp -d)
 pushd ${TMP_DIR}
-    curl -L -O https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
-    unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+    if [ ${IMAGE_BOOTSTRAP} == "true" ]; then
+        curl -L -O $(jq -r .[0].remote_url ${PWD_DIR}/files/versions.json | sed "s;VERSION;${VERSION};g")
+    else
+        curl -u "${REPOSITORY_USERNAME}:${REPOSITORY_PASSWORD}" -L -O $(jq -r .[0].local_url ${PWD_DIR}/files/versions.json | sed "s;VERSION;${VERSION};g" | sed "s;REPOSITORY_URL;${REPOSITORY_URL};" | sed "s;REPOSITORY_RAW_REPO;${REPOSITORY_RAW_REPO};")
+    fi
+    unzip ./$(jq -r .[0].file_name ${PWD_DIR}/files/versions.json | sed "s;VERSION;${VERSION};")
 popd
 mv -v ${TMP_DIR}/terraform ${CONTAINER_PATH}/usr/local/bin/terraform
 chmod -v 0755 ${CONTAINER_PATH}/usr/local/bin/terraform
