6e851930e4afbc129d739f07d626238ca208f30d
Marcin Skarbek <git@skarbek.name>
2022-08-04 11:15:47 +0200

    refactor(vault): add local artefact source

--- vault/build.sh
+++ vault/build.sh
@@ -2,14 +2,19 @@
 set -eu
 
 source ../meta/common.sh
-source ./files/VERSIONS
 
 container_create systemd ${1}
 
+VERSION=$(jq -r .[0].version ./files/versions.json)
+PWD_DIR=$(pwd)
 TMP_DIR=$(mktemp -d)
 pushd ${TMP_DIR}
-    curl -L -O https://releases.hashicorp.com/vault/${VAULT_VERSION}/vault_${VAULT_VERSION}_linux_amd64.zip
-    unzip vault_${VAULT_VERSION}_linux_amd64.zip
+    if [ ${IMAGE_BOOTSTRAP} == "true" ]; then
+        curl -L -O $(jq -r .[0].remote_url ${PWD_DIR}/files/versions.json | sed "s;VERSION;${VERSION};g")
+    else
+        curl -u "${REPOSITORY_USERNAME}:${REPOSITORY_PASSWORD}" -L -O $(jq -r .[0].local_url ${PWD_DIR}/files/versions.json | sed "s;VERSION;${VERSION};g" | sed "s;REPOSITORY_URL;${REPOSITORY_URL};" | sed "s;REPOSITORY_RAW_REPO;${REPOSITORY_RAW_REPO};")
+    fi
+    unzip ./$(jq -r .[0].file_name ${PWD_DIR}/files/versions.json | sed "s;VERSION;${VERSION};")
 popd
 mv -v ${TMP_DIR}/vault ${CONTAINER_PATH}/usr/local/bin/vault
 chmod -v 0755 ${CONTAINER_PATH}/usr/local/bin/vault
