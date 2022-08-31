dcedf1ec8e468ec57faf868dfc0fccf8667ba5c6
Marcin Skarbek <git@skarbek.name>
2022-08-31 18:15:50 +0200

    chore: new releases

--- minio/build.sh
+++ minio/build.sh
@@ -11,8 +11,8 @@ if [ ${IMAGE_BOOTSTRAP} == "true" ]; then
     curl -L -o ${CONTAINER_PATH}/usr/local/bin/minio $(jq -r .[0].remote_url ./files/versions.json | sed "s;VERSION;${MINIO_VERSION};g")
     curl -L -o ${CONTAINER_PATH}/usr/local/bin/mcli $(jq -r .[1].remote_url ./files/versions.json | sed "s;VERSION;${MCLI_VERSION};g")
 else
-    curl -u "${REPOSITORY_USERNAME}:${REPOSITORY_PASSWORD}" -L -o ${CONTAINER_PATH}/usr/local/bin/minio $(jq -r .[0].local_url ./files/versions.json | sed "s;VERSION;${VERSION};g" | sed "s;REPOSITORY_URL;${REPOSITORY_URL};" | sed "s;REPOSITORY_RAW_REPO;${REPOSITORY_RAW_REPO};")
-    curl -u "${REPOSITORY_USERNAME}:${REPOSITORY_PASSWORD}" -L -o ${CONTAINER_PATH}/usr/local/bin/mcli $(jq -r .[1].local_url ./files/versions.json | sed "s;VERSION;${VERSION};g" | sed "s;REPOSITORY_URL;${REPOSITORY_URL};" | sed "s;REPOSITORY_RAW_REPO;${REPOSITORY_RAW_REPO};")
+    curl -u "${REPOSITORY_USERNAME}:${REPOSITORY_PASSWORD}" -L -o ${CONTAINER_PATH}/usr/local/bin/minio $(jq -r .[0].local_url ./files/versions.json | sed "s;VERSION;${MINIO_VERSION};g" | sed "s;REPOSITORY_URL;${REPOSITORY_URL};" | sed "s;REPOSITORY_RAW_REPO;${REPOSITORY_RAW_REPO};")
+    curl -u "${REPOSITORY_USERNAME}:${REPOSITORY_PASSWORD}" -L -o ${CONTAINER_PATH}/usr/local/bin/mcli $(jq -r .[1].local_url ./files/versions.json | sed "s;VERSION;${MCLI_VERSION};g" | sed "s;REPOSITORY_URL;${REPOSITORY_URL};" | sed "s;REPOSITORY_RAW_REPO;${REPOSITORY_RAW_REPO};")
 fi
 chmod -v 0755 ${CONTAINER_PATH}/usr/local/bin/{minio,mcli}
 
--- step-ca/build.sh
+++ step-ca/build.sh
@@ -2,14 +2,20 @@
 set -eu
 
 source ../meta/common.sh
-source ./files/VERSIONS
 
 container_create systemd ${1}
 
+STEPCA_VERSION=$(jq -r .[0].version ./files/versions.json)
+STEPCLI_VERSION=$(jq -r .[1].version ./files/versions.json)
 TMP_DIR=$(mktemp -d)
 pushd ${TMP_DIR}
-    curl -L https://github.com/smallstep/certificates/releases/download/v${STEPCA_VERSION}/step-ca_linux_${STEPCA_VERSION}_amd64.tar.gz|tar xzv
-    curl -L https://github.com/smallstep/cli/releases/download/v${STEPCLI_VERSION}/step_linux_${STEPCLI_VERSION}_amd64.tar.gz|tar xzv
+if [ ${IMAGE_BOOTSTRAP} == "true" ]; then
+    curl -L $(jq -r .[0].remote_url ./files/versions.json | sed "s;VERSION;${STEPCA_VERSION};g") | tar xzv
+    curl -L $(jq -r .[1].remote_url ./files/versions.json | sed "s;VERSION;${STEPCLI_VERSION};g") | tar xzv
+else
+    curl -u "${REPOSITORY_USERNAME}:${REPOSITORY_PASSWORD}" -L $(jq -r .[0].local_url ./files/versions.json | sed "s;VERSION;${STEPCA_VERSION};g" | sed "s;REPOSITORY_URL;${REPOSITORY_URL};" | sed "s;REPOSITORY_RAW_REPO;${REPOSITORY_RAW_REPO};") | tar xzv
+    curl -u "${REPOSITORY_USERNAME}:${REPOSITORY_PASSWORD}" -L $(jq -r .[1].local_url ./files/versions.json | sed "s;VERSION;${STEPCLI_VERSION};g" | sed "s;REPOSITORY_URL;${REPOSITORY_URL};" | sed "s;REPOSITORY_RAW_REPO;${REPOSITORY_RAW_REPO};") | tar xzv
+fi
 popd
 mv -v ${TMP_DIR}/step-ca_${STEPCA_VERSION}/bin/step-ca ${CONTAINER_PATH}/usr/local/bin/step-ca
 mv -v ${TMP_DIR}/step_${STEPCLI_VERSION}/bin/step ${CONTAINER_PATH}/usr/local/bin/step
