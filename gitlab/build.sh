9c7de2a0e76c27413352abb7b7ab542f4f083cdb
Marcin Skarbek <git@skarbek.name>
2022-11-16 17:11:03 +0100

    refactor(gitlab): drop ee, rewrite donload

--- gitlab/build.sh
+++ gitlab/build.sh
@@ -2,25 +2,19 @@
 set -eu
 
 source ../meta/common.sh
-source ./files/VERSIONS
 
 container_create openssh ${1}
 
-if [ -z ${GITLAB_TYPE} ] || [ ${GITLAB_TYPE} != "ee" ]; then
-    GITLAB_TYPE="ce"
-fi
-
 dnf_cache
-if [ ${IMAGE_BOOTSTRAP} == "true" ]; then
-    cp -v ./files/gitlab-${GITLAB_TYPE}.repo /etc/yum.repos.d/gitlab-${GITLAB_TYPE}.repo
-    cp -v ./files/gitlab-${GITLAB_TYPE}.repo ${CONTAINER_PATH}/etc/yum.repos.d/gitlab-${GITLAB_TYPE}.repo
-fi
 dnf_install "hostname perl policycoreutils policycoreutils-python-utils checkpolicy git libxcrypt-compat"
+VERSION=$(jq -r .[0].version ./files/versions.json)
 TMP_DIR=$(mktemp -d)
-pushd ${TMP_DIR}
-    dnf download gitlab-${GITLAB_TYPE}-${GITALB_VERSION}
-popd
-rpm -ivh --noscripts --root=${CONTAINER_PATH} ${TMP_DIR}/gitlab-${GITLAB_TYPE}-${GITALB_VERSION}-*.el8.x86_64.rpm
+if [ ${IMAGE_BOOTSTRAP} == "true" ]; then
+    curl -L -o ${TMP_DIR}/$(jq -r .[0].file_name ./files/versions.json | sed "s;VERSION;${VERSION};g") $(jq -r .[0].remote_url ./files/versions.json | sed "s;VERSION;${VERSION};g")
+else
+    curl -u "${REPOSITORY_USERNAME}:${REPOSITORY_PASSWORD}" -L -o ${TMP_DIR}/$(jq -r .[0].file_name ./files/versions.json | sed "s;VERSION;${VERSION};g") $(jq -r .[0].local_url ./files/versions.json | sed "s;VERSION;${VERSION};g" | sed "s;REPOSITORY_URL;${REPOSITORY_URL};" | sed "s;REPOSITORY_RAW_REPO;${REPOSITORY_RAW_REPO};")
+fi
+rpm -ivh --noscripts --root=${CONTAINER_PATH} ${TMP_DIR}/$(jq -r .[0].file_name ./files/versions.json | sed "s;VERSION;${VERSION};g")
 rm -vrf ${TMP_DIR}
 dnf_cache_clean
 dnf_clean
