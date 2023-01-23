003a1880a2be09d4b2c18c49fdd0a31b1089ce4d
Marcin Skarbek <git@skarbek.name>
2023-01-23 01:10:37 +0100

    feat(keycloak): update to quarkus-based version

--- keycloak/build.sh
+++ keycloak/build.sh
@@ -2,32 +2,32 @@
 set -eu
 
 source ../meta/common.sh
-source ./files/VERSIONS
 
-container_create openjdk11-jre ${1}
+container_create openjdk17-jre ${1}
 
+VERSION=$(jq -r .[0].version ./files/versions.json)
+PWD_DIR=$(pwd)
 TMP_DIR=$(mktemp -d)
 pushd ${TMP_DIR}
-    curl -L https://github.com/keycloak/keycloak/releases/download/${KEYCLOAK_VERSION}/keycloak-${KEYCLOAK_VERSION}.tar.gz|tar xzv
+    if [ ${IMAGE_BOOTSTRAP} == "true" ]; then
+        curl -L $(jq -r .[0].remote_url ${PWD_DIR}/files/versions.json | sed "s;VERSION;${VERSION};g") | tar xzvA
+    else
+        curl -u "${REPOSITORY_USERNAME}:${REPOSITORY_PASSWORD}" -L $(jq -r .[0].local_url ${PWD_DIR}/files/versions.json | sed "s;VERSION;${VERSION};g" | sed "s;REPOSITORY_URL;${REPOSITORY_URL};" | sed "s;REPOSITORY_RAW_REPO;${REPOSITORY_RAW_REPO};") | tar xzvA
+    fi
 popd
-mv -v ${TMP_DIR}/keycloak-${KEYCLOAK_VERSION} ${CONTAINER_PATH}/usr/lib/keycloak
-chown -R root:root ${CONTAINER_PATH}/usr/lib/keycloak
+pushd ${CONTAINER_PATH}/usr/lib
+    mv -v ${TMP_DIR}/keycloak-${VERSION} ./
+    ln -s ./keycloak-${VERSION} keycloak
+    mv -v ${CONTAINER_PATH}/usr/lib/keycloak/conf ${CONTAINER_PATH}/usr/share/keycloak
+popd
 rm -vrf ${TMP_DIR}
 
 rsync_rootfs
-sed -i "s/POSTGRESQL_JDBC_VERSION/${POSTGRESQL_JDBC_VERSION}/" ${CONTAINER_PATH}/usr/lib/keycloak/modules/system/layers/keycloak/org/postgresql/main/module.xml
-
-curl -L https://jdbc.postgresql.org/download/postgresql-${POSTGRESQL_JDBC_VERSION}.jar\
- -o ${CONTAINER_PATH}/usr/lib/keycloak/modules/system/layers/keycloak/org/postgresql/main/postgresql-${POSTGRESQL_JDBC_VERSION}.jar
 
 buildah run --network none ${CONTAINER_UUID} systemctl enable\
  keycloak.service
 
-# Standalone mode
-buildah config --volume /usr/lib/keycloak/standalone/data ${CONTAINER_UUID}
-buildah config --volume /usr/lib/keycloak/standalone/log ${CONTAINER_UUID}
-# Domain mode
-#buildah config --volume /usr/lib/keycloak/domain/data ${CONTAINER_UUID}
-#buildah config --volume /usr/lib/keycloak/domain/log ${CONTAINER_UUID}
+buildah config --volume /usr/lib/keycloak/conf ${CONTAINER_UUID}
+buildah config --volume /usr/lib/keycloak/data ${CONTAINER_UUID}
 
 container_commit keycloak ${IMAGE_TAG}
