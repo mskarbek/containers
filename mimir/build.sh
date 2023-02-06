e3a6da763963c0c75a78bad882a8758714166d2d
Marcin Skarbek <git@skarbek.name>
2023-02-06 12:04:37 +0100

    chore: new releases

--- mimir/build.sh
+++ mimir/build.sh
@@ -9,15 +9,17 @@ VERSION=$(jq -r .[0].version ./files/versions.json)
 if [ ${IMAGE_BOOTSTRAP} == "true" ]; then
     curl -L -o ${CONTAINER_PATH}/usr/local/bin/metaconvert $(jq -r .[0].remote_url ./files/versions.json | sed "s;VERSION;${VERSION};g")
     curl -L -o ${CONTAINER_PATH}/usr/local/bin/mimir $(jq -r .[1].remote_url ./files/versions.json | sed "s;VERSION;${VERSION};g")
-    curl -L -o ${CONTAINER_PATH}/usr/local/bin/mimirtool $(jq -r .[2].remote_url ./files/versions.json | sed "s;VERSION;${VERSION};g")
-    curl -L -o ${CONTAINER_PATH}/usr/local/bin/query-tee $(jq -r .[3].remote_url ./files/versions.json | sed "s;VERSION;${VERSION};g")
+    curl -L -o ${CONTAINER_PATH}/usr/local/bin/mimir-continuous-test $(jq -r .[2].remote_url ./files/versions.json | sed "s;VERSION;${VERSION};g")
+    curl -L -o ${CONTAINER_PATH}/usr/local/bin/mimirtool $(jq -r .[3].remote_url ./files/versions.json | sed "s;VERSION;${VERSION};g")
+    curl -L -o ${CONTAINER_PATH}/usr/local/bin/query-tee $(jq -r .[4].remote_url ./files/versions.json | sed "s;VERSION;${VERSION};g")
 else
     curl -u "${REPOSITORY_USERNAME}:${REPOSITORY_PASSWORD}" -L -o ${CONTAINER_PATH}/usr/local/bin/metaconvert $(jq -r .[0].local_url ./files/versions.json | sed "s;VERSION;${VERSION};g" | sed "s;REPOSITORY_URL;${REPOSITORY_URL};" | sed "s;REPOSITORY_RAW_REPO;${REPOSITORY_RAW_REPO};")
     curl -u "${REPOSITORY_USERNAME}:${REPOSITORY_PASSWORD}" -L -o ${CONTAINER_PATH}/usr/local/bin/mimir $(jq -r .[1].local_url ./files/versions.json | sed "s;VERSION;${VERSION};g" | sed "s;REPOSITORY_URL;${REPOSITORY_URL};" | sed "s;REPOSITORY_RAW_REPO;${REPOSITORY_RAW_REPO};")
-    curl -u "${REPOSITORY_USERNAME}:${REPOSITORY_PASSWORD}" -L -o ${CONTAINER_PATH}/usr/local/bin/mimirtool $(jq -r .[2].local_url ./files/versions.json | sed "s;VERSION;${VERSION};g" | sed "s;REPOSITORY_URL;${REPOSITORY_URL};" | sed "s;REPOSITORY_RAW_REPO;${REPOSITORY_RAW_REPO};")
-    curl -u "${REPOSITORY_USERNAME}:${REPOSITORY_PASSWORD}" -L -o ${CONTAINER_PATH}/usr/local/bin/query-tee $(jq -r .[3].local_url ./files/versions.json | sed "s;VERSION;${VERSION};g" | sed "s;REPOSITORY_URL;${REPOSITORY_URL};" | sed "s;REPOSITORY_RAW_REPO;${REPOSITORY_RAW_REPO};")
+    curl -u "${REPOSITORY_USERNAME}:${REPOSITORY_PASSWORD}" -L -o ${CONTAINER_PATH}/usr/local/bin/mimir-continuous-test $(jq -r .[2].local_url ./files/versions.json | sed "s;VERSION;${VERSION};g" | sed "s;REPOSITORY_URL;${REPOSITORY_URL};" | sed "s;REPOSITORY_RAW_REPO;${REPOSITORY_RAW_REPO};")
+    curl -u "${REPOSITORY_USERNAME}:${REPOSITORY_PASSWORD}" -L -o ${CONTAINER_PATH}/usr/local/bin/mimirtool $(jq -r .[3].local_url ./files/versions.json | sed "s;VERSION;${VERSION};g" | sed "s;REPOSITORY_URL;${REPOSITORY_URL};" | sed "s;REPOSITORY_RAW_REPO;${REPOSITORY_RAW_REPO};")
+    curl -u "${REPOSITORY_USERNAME}:${REPOSITORY_PASSWORD}" -L -o ${CONTAINER_PATH}/usr/local/bin/query-tee $(jq -r .[4].local_url ./files/versions.json | sed "s;VERSION;${VERSION};g" | sed "s;REPOSITORY_URL;${REPOSITORY_URL};" | sed "s;REPOSITORY_RAW_REPO;${REPOSITORY_RAW_REPO};")
 fi
-chmod -v 0755 ${CONTAINER_PATH}/usr/local/bin/{metaconvert,mimir,mimirtool,query-tee}
+chmod -v 0755 ${CONTAINER_PATH}/usr/local/bin/{metaconvert,mimir,mimir-continuous-test,mimirtool,query-tee}
 
 rsync_rootfs
 
