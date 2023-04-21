13dec05d578947018685bf729898d37ddb6ae925
Marcin Skarbek <git@skarbek.name>
2023-04-21 12:12:48 +0200

    feat(rustdesk): new image

---
+++ rustdesk/build.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -eu
+
+source ../meta/common.sh
+
+container_create systemd ${1}
+
+VERSION=$(jq -r .[0].version ./files/versions.json)
+PWD_DIR=$(pwd)
+TMP_DIR=$(mktemp -d)
+pushd ${TMP_DIR}
+    if [ ${IMAGE_BOOTSTRAP} == "true" ]; then
+        curl -LO $(jq -r .[0].remote_url ${PWD_DIR}/files/versions.json | sed "s;VERSION;${VERSION};g")
+    else
+        curl -u "${REPOSITORY_USERNAME}:${REPOSITORY_PASSWORD}" -LO $(jq -r .[0].local_url ${PWD_DIR}/files/versions.json | sed "s;VERSION;${VERSION};g" | sed "s;REPOSITORY_URL;${REPOSITORY_URL};" | sed "s;REPOSITORY_RAW_REPO;${REPOSITORY_RAW_REPO};")
+    fi
+    unzip $(jq -r .[0].file_name ${PWD_DIR}/files/versions.json | sed "s;VERSION;${VERSION};g")
+popd
+mv -v ${TMP_DIR}/amd64/{hbbr,hbbs,rustdesk-utils} ${CONTAINER_PATH}/usr/local/bin/
+chmod -v 0755 ${CONTAINER_PATH}/usr/local/bin/{hbbr,hbbs,rustdesk-utils}
+rm -vrf ${TMP_DIR}
+
+rsync_rootfs
+
+buildah run --network none ${CONTAINER_UUID} systemctl enable\
+ rustdesk-hbbr.service\
+ rustdesk-hbbs.service
+
+container_commit rustdesk ${IMAGE_TAG}
